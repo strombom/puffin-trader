b0f6a2b3be6f511290835cb2d0cc9318546e4538
Johan <strombom@gmail.com>
2019-07-15 23:57:17 +0200

    Fix steps

--- database/bitmex_make_minute_data.py
+++ database/bitmex_make_minute_data.py
@@ -12,19 +12,21 @@ def sec15_datetime(dt):
 
 
 class IntervalTable(tables.IsDescription):
-    vol     = tables.Float32Col(pos =  0)
-    sell_01 = tables.Float32Col(pos =  1)
-    sell_02 = tables.Float32Col(pos =  2)
-    sell_05 = tables.Float32Col(pos =  3)
-    sell_1  = tables.Float32Col(pos =  4)
-    sell_2  = tables.Float32Col(pos =  5)
-    sell_5  = tables.Float32Col(pos =  6)
-    buy_01  = tables.Float32Col(pos =  7)
-    buy_02  = tables.Float32Col(pos =  8)
-    buy_05  = tables.Float32Col(pos =  9)
-    buy_1   = tables.Float32Col(pos = 10)
-    buy_2   = tables.Float32Col(pos = 11)
-    buy_5   = tables.Float32Col(pos = 12)
+    timestamp = tables.UInt64Col (pos =  0)
+    vol       = tables.Float32Col(pos =  1)
+    price     = tables.Float32Col(pos =  2)
+    sell_01   = tables.Float32Col(pos =  3)
+    sell_02   = tables.Float32Col(pos =  4)
+    sell_05   = tables.Float32Col(pos =  5)
+    sell_1    = tables.Float32Col(pos =  6)
+    sell_2    = tables.Float32Col(pos =  7)
+    sell_5    = tables.Float32Col(pos =  8)
+    buy_01    = tables.Float32Col(pos =  9)
+    buy_02    = tables.Float32Col(pos = 10)
+    buy_05    = tables.Float32Col(pos = 11)
+    buy_1     = tables.Float32Col(pos = 12)
+    buy_2     = tables.Float32Col(pos = 13)
+    buy_5     = tables.Float32Col(pos = 14)
 
 
 class BitmexRaw:
@@ -39,6 +41,11 @@ class BitmexRaw:
         self.current_timeperiod = sec15_datetime(self.get_first_timestamp())
         self.row_idx = 0
 
+        self.row_idx = 1747
+        self.current_timeperiod = datetime.strptime("2015-10-07 12:45:45", "%Y-%m-%d %H:%M:%S")
+
+        self.a = 0
+
     def get_first_timestamp(self):
         return datetime.fromtimestamp(self.xbtusd_ticks_table[0][0] / 1000000)
 
@@ -48,18 +55,18 @@ class BitmexRaw:
 
     def get_interval_data(self):
 
-        self.row_idx = 1747
-        self.current_timeperiod = datetime.strptime("2015-10-07 12:45:45", "%Y-%m-%d %H:%M:%S")
-
         start_idx = self.row_idx
         timeperiod_end = datetime.timestamp(self.current_timeperiod + interval_length) * 1000000
 
+        volume = 0
         count = 0
         while True:
             timestamp = self.xbtusd_ticks_table.cols.timestamp[start_idx + count]
             #print(">>>", timestamp, datetime.fromtimestamp(timestamp / 1000000))
             if timestamp >= timeperiod_end:
                 break
+
+            volume += self.xbtusd_ticks_table.cols.volume[start_idx + count]
             count += 1
 
         self.row_idx += count
@@ -70,13 +77,18 @@ class BitmexRaw:
 
         buys = []
         sells = []
+        buy_vol = 0
+        sell_vol = 0
 
         for idx in range(count):
             row = [self.xbtusd_ticks_table.cols.price[start_idx + idx], self.xbtusd_ticks_table.cols.volume[start_idx + idx]]
+            vol = self.xbtusd_ticks_table.cols.volume[start_idx + idx]
             if self.xbtusd_ticks_table.cols.buy[start_idx + idx]:
                 buys.append(row)
+                buy_vol += vol
             else:
                 sells.append(row)
+                sell_vol += vol
 
         accum_buy = 0
         accum_sell = 0
@@ -87,36 +99,44 @@ class BitmexRaw:
         buy_prices = [0, 0, 0, 0, 0, 0]
         sell_prices = [0, 0, 0, 0, 0, 0]
         steps = [0.1, 0.2, 0.5, 1.0, 2.0, 5.0]
+
+        #print("sell")
         step_idx = 0
-
-        print("sell")
         for sell in sells:
+            #print(" ", sell)
             accum_sell += sell[1]
-            while step_idx < len(steps) - 1 and accum_sell > steps[step_idx + 1]:
+            while step_idx < len(steps) and accum_sell > steps[step_idx]:
                 sell_prices[step_idx] = sell[0]
-                print("  inc", step_idx)
                 step_idx += 1
-            sell_prices[step_idx] = sell[0]
-            print(" ", sell, accum_sell, steps[step_idx], step_idx)
-
         print("sell prices", sell_prices)
 
-        print("buy")
+        #print("buy")
+        step_idx = 0
         for buy in buys:
+            #print(" ", buy)
             accum_buy += buy[1]
-            print(" ", buy, accum_buy)
+            while step_idx < len(steps) and accum_buy > steps[step_idx]:
+                buy_prices[step_idx] = buy[0]
+                step_idx += 1
+        print("buy prices", buy_prices)
 
         prices = {}
 
+        last_price = self.xbtusd_ticks_table.cols.price[self.row_idx]
 
         print("row_idx", start_idx - count)
+        print("last_price, volume", last_price, volume, buy_vol, sell_vol)
         print("current_timeperiod", self.current_timeperiod - interval_length)
         print("==========")
 
-        quit()
+        #return timestamp, 
 
-        if count == 0:
-            return
+        self.a += 1
+        if self.a == 1:
+            quit()
+
+        #if count == 0:
+        #    return
 
 
 
