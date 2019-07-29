0e52cfcdc036fa7418ac31d4c629e4177d1469a2
Johan Strömbom <strombom@gmail.com>
2019-07-29 23:15:34 +0200

    Exit handler

--- database/bitmex_make_minute_data.py
+++ database/bitmex_make_minute_data.py
@@ -3,6 +3,8 @@ import sys
 import atexit
 import signal
 import tables
+import threading
+from pynput import keyboard
 from datetime import datetime
 from datetime import timedelta
 from operator import itemgetter
@@ -178,34 +180,71 @@ class BitmexIntervals:
         self.save_start_timeperiod_and_row_idx(force = True)
         self.xbtusd_15sec_table.flush()
 
+running = True
 
-bitmex_intervals = BitmexIntervals()
-bitmex_raw       = BitmexRaw(bitmex_intervals)
+def minute_data_loop():
+    global running
+    bitmex_intervals = BitmexIntervals()
+    bitmex_raw       = BitmexRaw(bitmex_intervals)
+    count = 0
+    while running:
+        try:
+            timestamp, last_price, vol_sell, vol_buy, prices_buy, prices_sell = bitmex_raw.get_interval_data()
+            bitmex_intervals.append(timestamp, last_price, vol_sell, vol_buy, prices_buy, prices_sell)
+            
+            count += 1
+            if count == 10000:
+                count = 0
+                bitmex_intervals.flush()
+                print(timestamp, last_price, vol_sell, vol_buy, prices_buy, prices_sell)
+                print("start", bitmex_intervals.raw_start_timeperiod, bitmex_intervals.raw_start_row_idx)
+                print("===")
+        
+        except IndexError:
+            bitmex_intervals.flush()
+            break
 
+    bitmex_intervals.flush()
+    print("minute data loop end")
+
+def save_on_key_exit(key):
+    #print('{0} press'.format(key))
+    if key == keyboard.Key.esc:
+        print('Exit key, saving.')
+        global running
+        running = False
+
+def key_listener_loop():
+    global running
+    with keyboard.Listener(on_press=save_on_key_exit) as listener:
+        listener.join()
+    print("key listener loop exit")
+
+minute_data_thread  = threading.Thread(target=minute_data_loop)
+key_listener_thread = threading.Thread(target=key_listener_loop)
+key_listener_thread.daemon = True
+
+
+"""
 def signal_handler(sig, frame):
     print('Exit signal, ctrl+c.')
-    sys.exit(0)
+    #sys.exit(0)
+    global running
+    running = False
+    #minute_data_thread.join()
 
 def save_on_exit():
     print('Exit, saving.')
-    bitmex_intervals.flush()
+    global running
+    running = False
+    #bitmex_intervals.flush()
+    minute_data_thread.join()
+    quit()
 
 signal.signal(signal.SIGINT, signal_handler)
 atexit.register(save_on_exit)
+"""
 
-count = 0
-while True:
-    try:
-        timestamp, last_price, vol_sell, vol_buy, prices_buy, prices_sell = bitmex_raw.get_interval_data()
-        bitmex_intervals.append(timestamp, last_price, vol_sell, vol_buy, prices_buy, prices_sell)
-        
-        count += 1
-        if count == 1000:
-            count = 0
-            bitmex_intervals.flush()
-            print(timestamp, last_price, vol_sell, vol_buy, prices_buy, prices_sell)
-            print("start", bitmex_intervals.raw_start_timeperiod, bitmex_intervals.raw_start_row_idx)
-            print("===")
-    
-    except IndexError:
-        quit()
+minute_data_thread.start()
+key_listener_thread.start()
+minute_data_thread.join()
