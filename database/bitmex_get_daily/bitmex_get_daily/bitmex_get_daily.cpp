c3772bfc1306dc09e1db9e6a0a8d0760f4595fe5
Johan Strömbom <strombom@gmail.com>
2019-10-26 23:12:18 +0200

    Datestring

--- database/bitmex_get_daily/bitmex_get_daily/bitmex_get_daily.cpp
+++ database/bitmex_get_daily/bitmex_get_daily/bitmex_get_daily.cpp
@@ -7,9 +7,12 @@
 #include <sstream>
 #include <string>
 
+#include "boost/date_time/gregorian/gregorian.hpp"
 #include "curl/curl.h"
 
-size_t download_daily_callback(void* ptr, size_t size, size_t count, void* stream)
+//using namespace boost;
+
+size_t download_file_callback(void* ptr, size_t size, size_t count, void* stream)
 {
 	static void* stream_id = 0;
 	static int download_count = 0;
@@ -34,7 +37,7 @@ size_t download_daily_callback(void* ptr, size_t size, size_t count, void* strea
 	return count;
 }
 
-void download_daily(const std::string& url) {
+void download_file(const std::string& url) {
 	std::stringstream download_data;
 
 	curl_global_init(CURL_GLOBAL_ALL);
@@ -42,7 +45,7 @@ void download_daily(const std::string& url) {
 	if (curl) {
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, FALSE);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &download_data);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, download_daily_callback);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, download_file_callback);
 		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
 		printf("Downloading 0 MB");
@@ -67,11 +70,21 @@ void download_daily(const std::string& url) {
 	download_data.seekg(0, std::stringstream::end);
 	unsigned int length = (int) download_data.tellg();
 	printf("file size %d\n", length);
+}
 
-
+std::string make_url(boost::gregorian::date date)
+{	
+	std::locale loc(std::cout.getloc(), new boost::date_time::date_facet < boost::gregorian::date, char>("%Y%m%d"));
+	std::stringstream url;
+	url.imbue(loc);
+	url << "https://s3-eu-west-1.amazonaws.com/public.bitmex.com/data/trade/" << date << ".csv.gz";
+	return url.str();
 }
 
 int main()
 {
-	download_daily("https://s3-eu-west-1.amazonaws.com/public.bitmex.com/data/trade/20171121.csv.gz");
+	boost::gregorian::date date {2017, 11, 21};
+	std::string url = make_url(date);
+	printf("Downloading URL: %s\n", url.c_str());
+	download_file(url);
 }
