4c236b4d9f0a46807bf5c2929f7345a7d5fd77a7
Johan Strombom <strombom@gmail.com>
2020-01-17 17:35:10 +0100

    Plot

--- tmp/plot.py
+++ tmp/plot.py
@@ -3,19 +3,29 @@ import matplotlib.pyplot as plt
 import torch
 
 for tensor_name in ("past_observations", "future_positives", "future_negatives"):
-
+    titles = ("T0", "T+10", "T+20")
     tensor = torch.load(tensor_name + ".tensor")
-    print(tensor_name, tensor.size())
+    B, C, N, O = 0, 1, 2, 3 # 2x3x4x160
+    nrows, ncols = tensor.size()[B], tensor.size()[N]
+    fig, ax_rows = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=True)
+    for row_idx, ax_row in enumerate(ax_rows):
+        for col_idx, ax in enumerate(ax_row):
+            ax.plot(tensor[row_idx, 0, col_idx, :])
+            ax.set_ylim(-0.02, 0.02)
+            if col_idx == 0:
+                ax.set_ylabel(titles[row_idx])
+    fig.set_size_inches(ncols*4, nrows*4)
+    fig.suptitle(tensor_name, fontsize=10)
+    plt.savefig(tensor_name + ".png")
 
-    # 2x3x4x256
-    B, C, N, F = 0, 1, 2, 3
+quit()
 
+for tensor_name in ("past_observations", "future_positives", "future_negatives"):
     titles = ("Price", "Buy vol", "Sell vol")
-
+    tensor = torch.load(tensor_name + ".tensor")
+    B, C, N, O = 0, 1, 2, 3 # 160
     nrows, ncols = tensor.size()[C], tensor.size()[N]
-
     fig, ax_rows = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=False)
-
     for row_idx, ax_row in enumerate(ax_rows):
         for col_idx, ax in enumerate(ax_row):
             ax.plot(tensor[0, row_idx, col_idx, :])
@@ -30,25 +40,4 @@ for tensor_name in ("past_observations", "future_positives", "future_negatives")
     fig.suptitle(tensor_name, fontsize=10)
     plt.savefig(tensor_name + ".png")
 
-quit()
 
-
-for tensor_name in ("past_observations", "future_positives", "future_negatives"):
-
-    tensor = torch.load(tensor_name + ".tensor")
-    print(tensor_name, tensor.size())
-
-    # 2x3x4x256
-    B, C, N, F = 0, 1, 2, 3
-
-    nrows, ncols = tensor.size()[B], tensor.size()[N]
-
-    fig, ax_rows = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=True)
-
-    for row_idx, ax_row in enumerate(ax_rows):
-        for col_idx, ax in enumerate(ax_row):
-            ax.plot(tensor[row_idx, 0, col_idx, :])
-
-    fig.set_size_inches(ncols*4, nrows*4)
-    fig.suptitle(tensor_name, fontsize=10)
-    plt.savefig(tensor_name + ".png")
