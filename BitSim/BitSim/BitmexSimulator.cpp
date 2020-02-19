c901df3ac2d2c853f2a37895e4ee886ac5f5a042
Johan Strombom <strombom@gmail.com>
2020-02-20 00:50:03 +0100

    Timestamp end fix

--- BitSim/BitSim/BitmexSimulator.cpp
+++ BitSim/BitSim/BitmexSimulator.cpp
@@ -6,7 +6,12 @@
 
 void BitmexSimulator::reset(void)
 {
-    current_timestamp = DateTime::random_timestamp(intervals->get_timestamp_start(), intervals->get_timestamp_end(), BitSim::interval);
+    const auto timestamp_start = intervals->get_timestamp_start();
+    const auto timestamp_end = intervals->get_timestamp_end() - BitSim::Closer::closing_timeout;
+
+    current_timestamp = DateTime::random_timestamp(timestamp_start, timestamp_end, BitSim::interval);
+
+
 
     std::cout << DateTime::to_string(current_timestamp) << std::endl;
 
