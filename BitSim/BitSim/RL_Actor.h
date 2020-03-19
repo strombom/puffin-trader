bb84e48e996927dc7e4f1fd166de48bd46ae5411
Johan Strombom <strombom@gmail.com>
2020-03-19 11:09:01 +0100

    Critic networks

--- BitSim/BitSim/RL_Actor.h
+++ BitSim/BitSim/RL_Actor.h
@@ -11,7 +11,11 @@ class RL_Actor
 {
 public:
     RL_Actor(void) :
-        policy(TanhGaussianDistParams{ "policy", BitSim::Trader::state_dim, BitSim::Trader::action_dim })
+        policy(TanhGaussianDistParams{ "policy", BitSim::Trader::state_dim, BitSim::Trader::action_dim }),
+        vf(MultilayerPerceptron{ "vf", BitSim::Trader::state_dim, BitSim::Trader::action_dim }),
+        vf_target(MultilayerPerceptron{ "vf_target", BitSim::Trader::state_dim, BitSim::Trader::action_dim }),
+        qf_1(FlattenMultilayerPerceptron{ "qf_1", BitSim::Trader::state_dim, BitSim::Trader::action_dim }),
+        qf_2(FlattenMultilayerPerceptron{ "vf", BitSim::Trader::state_dim, BitSim::Trader::action_dim })
     {}
 
     RL_Action get_action(RL_State state);
@@ -19,5 +23,8 @@ public:
 
 private:
     TanhGaussianDistParams policy;
-    
+    MultilayerPerceptron vf;
+    MultilayerPerceptron vf_target;
+    FlattenMultilayerPerceptron qf_1;
+    FlattenMultilayerPerceptron qf_2;
 };
