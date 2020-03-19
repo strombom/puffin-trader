0cc3401b991e162033317f49d949627669cd93e0
Johan Strombom <strombom@gmail.com>
2020-03-19 11:07:10 +0100

    Remove Agent, move policy network to Actor

--- BitSim/BitSim/RL_Actor.h
+++ BitSim/BitSim/RL_Actor.h
@@ -3,12 +3,21 @@
 
 #include "RL_State.h"
 #include "RL_Action.h"
+#include "RL_Networks.h"
+#include "BitBotConstants.h"
 
 
 class RL_Actor
 {
 public:
+    RL_Actor(void) :
+        policy(TanhGaussianDistParams{ "policy", BitSim::Trader::state_dim, BitSim::Trader::action_dim })
+    {}
+
     RL_Action get_action(RL_State state);
     RL_Action get_random_action(void);
 
+private:
+    TanhGaussianDistParams policy;
+    
 };
