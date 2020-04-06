b1a03237743a743eea4c37af24b77b8eac8d6b91
Johan Strömbom <strombom@gmail.com>
2020-04-06 11:52:51 +0200

    Plot cartpole

--- tmp/plot_cartpole.py
+++ tmp/plot_cartpole.py
@@ -11,154 +11,48 @@ with open('C:\\development\\github\\puffin-trader\\tmp\\log\\cartpole_' + episod
     reader = csv.reader(csvfile)
     is_first = True
     for row in reader:
-        print(row)
         if is_first:
             titles = row
             is_first = False
         else:
             rows.append([float(i) for i in row])
 
-
-rows = np.array(rows)
-print(rows)
-
-
-
-quit()
-
-
-
-
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-import sys
-import csv
-
-episode_idx = sys.argv[1]
-
-titles = []
-rows = []
-
-with open('C:\\development\\github\\puffin-trader\\tmp\\log\\simulation_' + episode_idx + '.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    is_first = True
-    for row in reader:
-        if is_first:
-            titles = row
-            is_first = False
-        else:
-            rows.append([float(i) for i in row])
-
-
 rows = np.array(rows)
 
-def make_patch_spines_invisible(ax):
-    ax.set_frame_on(True)
-    ax.patch.set_visible(False)
-    for sp in ax.spines.values():
-        sp.set_visible(False)
-
-fig, host = plt.subplots(1, 1, figsize=(12, 10))
-fig.subplots_adjust(right=0.65)
-
-par1 = host.twinx()
-par2 = host.twinx()
-par3 = host.twinx()
-
-par2.spines["right"].set_position(("axes", 1.2))
-make_patch_spines_invisible(par2)
-par2.spines["right"].set_visible(True)
-
-par3.spines["right"].set_position(("axes", 1.4))
-make_patch_spines_invisible(par3)
-par3.spines["right"].set_visible(True)
-
-p1, = host.plot(rows[:,0], "b-", label="Last price")
-p2, = par1.plot(rows[:,2], "g-", label="Contracts")
-p3, = par2.plot(rows[:,3], "r-", label="Wallet")
-p4, = par3.plot(rows[:,4], "y-", label="UPNL")
-
-host.set_xlabel("Time")
-host.set_ylabel("Last price")
-par1.set_ylabel("Contracts")
-par2.set_ylabel("Wallet")
-par3.set_ylabel("UPNL")
-
-host.yaxis.label.set_color(p1.get_color())
-par1.yaxis.label.set_color(p2.get_color())
-par2.yaxis.label.set_color(p3.get_color())
-par3.yaxis.label.set_color(p4.get_color())
+from math import sin, cos
+import gizeh as gz
+import moviepy.editor as mpy
 
 
-#host.set_xlim(0, 2)
-#host.set_ylim(0, 2)
-#par1.set_ylim(0, 4)
-#par2.set_ylim(1, 65)
-#par3.set_ylim(1, 65)
-#par4.set_ylim(1, 65)
+width, height = 500, 300
+fps = 20
+duration = rows.shape[0] / fps
+first = True
 
+def make_frame(time):
+    row_idx = int(time * fps)
+    print("time", row_idx, time)
 
+    cart_position, cart_velocity, pole_angle, pole_velocity = rows[row_idx]
+    row_idx += 1
 
-plt.show()
+    surface = gz.Surface(width, height, bg_color=(1,1,1))
 
-quit()
+    x, y = width * (0.5 + cart_position * 5), height * 0.8
 
+    rect = gz.rectangle(lx=.15*width, ly=.15*height, xy=(x, y), fill=(0,1,0))
+    rect.draw(surface)
 
+    pole_length = height * 0.5
+    dx = sin(pole_angle) * pole_length
+    dy = -cos(pole_angle) * pole_length
 
-for col_idx in len(titles):
-    ax.plot(rows[col_idx])
+    line = gz.polyline(points=[(x,y), (x + dx,y + dy)], stroke_width=10,
+                         stroke=(1,0,0), fill=(1,0,0))
+    line.draw(surface)
 
+    return surface.get_npimage()
 
-print(titles)
-print(rows)
+clip = mpy.VideoClip(make_frame, duration=duration)
+clip.write_videofile('cartpole/cartpole_' + episode_idx + '.mp4', fps=25) # Many options...
 
-quit()
-
-
-for tensor_name in ("past_observations", "future_positives", "future_negatives"):
-    titles = ("Price", "Buy vol", "Sell vol")
-    tensor = torch.load('data/' + tensor_name + '_0.tensor').cpu()
-    B, C, N, O = 0, 1, 2, 3 # 160
-    nrows, ncols = tensor.size()[C], tensor.size()[N]
-    nrows, ncols = min(nrows, 3), min(ncols, 6)
-    fig, ax_rows = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=False)
-    for row_idx, ax_row in enumerate(ax_rows):
-        try:
-            iter(ax_row)
-        except:
-            ax_row = [ax_row]
-        for col_idx, ax in enumerate(ax_row):
-            ax.plot(tensor[0, row_idx, col_idx, :])
-            if row_idx == 0:
-                ax.set_ylim(-1, 1)
-            else:
-                ax.set_ylim(0, 1)
-            if col_idx == 0:
-                ax.set_ylabel(titles[row_idx])
-
-    fig.set_size_inches(ncols*4, nrows*4)
-    fig.suptitle(tensor_name, fontsize=10)
-    plt.savefig(tensor_name + ".png")
-
-quit()
-
-for tensor_name in ("past_observations", "future_positives", "future_negatives"):
-    titles = ("T0", "T+10", "T+20")
-    tensor = torch.load('data/' + tensor_name + '_0.tensor').cpu()
-    B, C, N, O = 0, 1, 2, 3 # 2x3x4x160
-    nrows, ncols = tensor.size()[B], tensor.size()[N]
-    nrows, ncols = min(nrows, 3), min(ncols, 4)
-    fig, ax_rows = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=True)
-    for row_idx, ax_row in enumerate(ax_rows):
-        for col_idx, ax in enumerate(ax_row):
-            ax.plot(tensor[row_idx, 0, col_idx, :])
-            ax.set_ylim(-1, 1)
-            if col_idx == 0:
-                ax.set_ylabel(titles[row_idx])
-    fig.set_size_inches(ncols*4, nrows*4)
-    fig.suptitle(tensor_name, fontsize=10)
-    plt.savefig(tensor_name + ".png")
-
-quit()
