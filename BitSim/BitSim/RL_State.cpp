84ddc05f729234215b0dc9236a3c194abb0dc1ca
Johan Strömbom <strombom@gmail.com>
2020-04-19 22:30:02 +0200

    Fix state variables

--- BitSim/BitSim/RL_State.cpp
+++ BitSim/BitSim/RL_State.cpp
@@ -18,44 +18,7 @@ bool RL_State::is_done(void) const
     return done;
 }
 
-//void RL_State::set_state(torch::Tensor features, double leverage)
-//{
-//    state = torch::cat({ features, torch::tensor({ leverage }) });
-//}
-
 torch::Tensor RL_State::to_tensor(void) const
 {
-    return torch::tensor({ reward, angle, velocity }).view({ 1, 3 });
+    return torch::tensor({ std::sin(angle), std::cos(angle), velocity }).view({ 1, 3 });
 }
-
-/*
-RL_State::RL_State(double reward, torch::Tensor features, double leverage) : done(false), reward(reward)
-{
-    state = torch::cat({ features, torch::tensor({ leverage }) });
-}
-
-void RL_State::set_done(void)
-{
-    done = true;
-}
-
-bool RL_State::is_done(void) const
-{
-    return done;
-}
-
-void RL_State::set_state(torch::Tensor features, double leverage)
-{
-    state = torch::cat({ features, torch::tensor({ leverage }) });
-}
-
-double RL_State::get_reward(void) const
-{
-    return reward;
-}
-
-torch::Tensor RL_State::to_tensor(void) const
-{
-    return state;
-}
-*/
--- BitSim/BitSim/RL_State.h
+++ BitSim/BitSim/RL_State.h
@@ -17,23 +17,3 @@ public:
 };
 
 using sptrRL_State = std::shared_ptr<RL_State>;
-
-/*
-class RL_State
-{
-public:
-    RL_State(double reward, torch::Tensor features, double leverage);
-
-    void set_done(void);
-    void set_state(torch::Tensor state, double leverage);
-
-    bool is_done(void) const;
-    double get_reward(void) const;
-    torch::Tensor to_tensor(void) const;
-
-private:
-    bool done;
-    double reward;
-    torch::Tensor state;
-};
-*/
