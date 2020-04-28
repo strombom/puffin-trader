c84eeadbb299111df107cbf12a87ac7f1ad93ac8
Johan Strombom <strombom@gmail.com>
2020-04-28 18:25:07 +0200

    Pendulum bugfix

--- BitSim/BitSim/PendulumSimulator.cpp
+++ BitSim/BitSim/PendulumSimulator.cpp
@@ -17,18 +17,25 @@ sptrRL_State PendulumSimulator::reset(int idx_episode)
     state = std::make_shared<RL_State>(
         0.0,                                        // reward
         Utils::random(-M_PI, M_PI),                 // angle
-        Utils::random(-1.0, 1.0)                    // velocity
+        Utils::random(-max_speed, max_speed)        // velocity
         );
     return state;
+}
+
+double normalize_angle(double angle)
+{
+    auto result = std::fmod(std::abs(angle) + 3.14, 2 * 3.14) - 3.14;;
+    if (angle < 0) {
+        result = -result;
+    }
+    return result;
 }
 
 sptrRL_State PendulumSimulator::step(sptrRL_Action action, bool last_step)
 {
     const auto torque = std::clamp(action->move, -max_torque, max_torque);
 
-    const auto normalized_angle = std::fmod(state->angle + M_PI, 2.0 * M_PI) - M_PI;
-    const auto cost = std::pow(normalized_angle, 2.0) + 0.1 * std::pow(state->velocity, 2.0) + 0.001 * std::pow(torque, 2.0);
-
+    const auto cost = std::pow(normalize_angle(state->angle), 2.0) + 0.1 * std::pow(state->velocity, 2.0) + 0.001 * std::pow(torque, 2.0);
     const auto new_velocity = state->velocity + (-3.0 * gravity / (2.0 * length) * std::sin(state->angle + M_PI) + 3.0 * torque / (mass * std::pow(length , 2.0))) * delta_time;
     const auto new_angle = state->angle + new_velocity * delta_time;
 
