36b85719f0d7471827022abd61c3883fe5453da6
Johan Strombom <strombom@gmail.com>
2020-05-22 16:45:39 +0200

    Plot cartpole 2D

--- tmp/plot_cartpole.py
+++ tmp/plot_cartpole.py
@@ -4,9 +4,13 @@ import sys
 import csv
 
 start = int(sys.argv[1])
-count = 1
+try:
+    stop = int(sys.argv[2])
+except:
+    stop = start
 
-for episode_idx in range(start,start+count):
+
+for episode_idx in range(start, stop + 1):
     titles = []
     rows = []
 
@@ -35,19 +39,20 @@ for episode_idx in range(start,start+count):
     def make_frame(time):
         row_idx = int(time * fps)
 
-        cart_position, cart_velocity, pole_angle, pole_velocity = rows[row_idx][0:4]
+        cart_x_position, cart_x_velocity, cart_y_position, cart_y_velocity, pole_angle, pole_velocity = rows[row_idx][0:6]
         row_idx += 1
 
         surface = gz.Surface(width, height, bg_color=(1,1,1))
 
-        x, y = width * (0.5 + cart_position * 0.1), height * 0.8
+        x, y = width * (0.5 + cart_x_position * 0.1), height * (0.5 - cart_y_position * 0.1)
+        #x, y = width * (0.5 + cart_position * 0.1), height * 0.8
 
         rect = gz.rectangle(lx=.15*width, ly=.15*height, xy=(x, y), fill=(0,1,0))
         rect.draw(surface)
 
         pole_length = height * 0.5
-        dx = sin(pole_angle) * pole_length
-        dy = -cos(pole_angle) * pole_length
+        dx = sin(-pole_angle + 3.14159) * pole_length
+        dy = -cos(-pole_angle + 3.14159) * pole_length
 
         line = gz.polyline(points=[(x,y), (x + dx,y + dy)], stroke_width=10,
                              stroke=(1,0,0), fill=(1,0,0))
@@ -56,5 +61,5 @@ for episode_idx in range(start,start+count):
         return surface.get_npimage()
 
     clip = mpy.VideoClip(make_frame, duration=duration)
-    clip.write_videofile('cartpole/cartpole_' + str(episode_idx) + '.mp4', fps=25) # Many options...
+    clip.write_videofile('cartpole/cartpole_' + str(episode_idx) + '.mp4', fps=60) # Many options...
 
