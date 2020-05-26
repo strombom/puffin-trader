bf1b3dec643bc5e7a52f130c407341ce77e42da3
Johan Strombom <strombom@gmail.com>
2020-05-26 20:37:13 +0200

    BitmexInterim extend tick data

--- BitBase/BitBase/BitmexInterim.cpp
+++ BitBase/BitBase/BitmexInterim.cpp
@@ -39,7 +39,7 @@ BitmexInterimState BitmexInterim::get_state(void)
 
 void BitmexInterim::shutdown(void)
 {
-    logger.info("BitmexDaily::shutdown");
+    logger.info("BitmexInterim::shutdown");
 
     {
         // Will not start new downloads after this section
@@ -96,150 +96,22 @@ void BitmexInterim::tick_data_worker(void)
                 auto trade = trade_api->trade_get(trade_symbol, boost::none, boost::none, trade_count, boost::none, boost::none, trade_start_time, boost::none);
                 auto results = trade.get();
 
-
                 auto tick_data = std::make_unique<Ticks>();
                 for (auto result : results) {
-                    std::wcout <<
-                        "Result: " << result->getTimestamp().to_string(utility::datetime::date_format::ISO_8601).c_str() <<
-                        "  " << result->getPrice() <<
-                        "  " << result->getSize() <<
-                        std::endl;
-
                     const auto timestamp_string = utility::conversions::to_utf8string(result->getTimestamp().to_string(utility::datetime::date_format::ISO_8601));
                     const auto timestamp = DateTime::to_time_point_ms(timestamp_string, "%FT%TZ");
-                    
-                    std::cout << "timestamp: " << timestamp.time_since_epoch().count() << std::endl;
-
-
-                    const auto direction = result->getTickDirection();
-
-                    std::wcout << "direction: " << direction << std::endl;
-                    //const time_point_ms timestamp, const float price, const float volume, const bool buy
-                    //tick_data->rows.push_back({ timestamp, price, volume, buy });
-                    std::cout << std::endl;
+                    const auto price = (float) result->getPrice();
+                    const auto volume = (float) result->getSize();
+                    const auto direction_string = utility::conversions::to_utf8string(result->getTickDirection());
+                    const auto direction = (direction_string.find("PlusTick") != std::string::npos) ? true : false;
+                    tick_data->rows.push_back({ timestamp, price, volume, direction });
                 }
-
-                std::cout << "Count: " << results.size() << std::endl;
-
-                std::wcout << "First w: " << DateTime::to_string_iso_8601(timestamps_next[symbol]) << std::endl;
-                std::cout << "First s: " << DateTime::to_string(timestamps_next[symbol]) << std::endl;
-
 
                 database->extend_tick_data(BitBase::Bitmex::exchange_name, symbol, std::move(tick_data), BitBase::Bitmex::first_timestamp);
-                tick_data_updated_callback();
             }
-
-            /*
-            {
-                auto slock = std::scoped_lock{ tick_data_mutex };
-                if (!tick_data_queue.empty()) {
-                    tick_data = std::move(tick_data_queue.front());
-                    tick_data_queue.pop_front();
-                }
-                else {
-                    break;
-                }
-            }
-
-            auto timer = Timer{};
-            auto symbol_names = std::unordered_set<std::string>{};
-            for (auto&& symbol_tick_data = tick_data->begin(); symbol_tick_data != tick_data->end(); ++symbol_tick_data) {
-                const auto symbol_name = symbol_tick_data->first;
-                symbol_names.insert(symbol_name);
-                database->extend_tick_data(BitBase::Bitmex::exchange_name, symbol_name, std::move(symbol_tick_data->second), BitBase::Bitmex::first_timestamp);
-            }
-            update_symbol_names(symbol_names);
-            tick_data_updated_callback();
-
-            logger.info("BitmexDaily::tick_data_worker tick_data appended to database (%d ms)", timer.elapsed().count() / 1000);
-
-            */
-
         }
+        tick_data_updated_callback();
+        logger.info("BitmexInterim::tick_data_worker tick_data appended to database");
     }
-    logger.info("BitmexDaily::tick_data_worker exit");
+    logger.info("BitmexInterim::tick_data_worker exit");
 }
-
-/*
-logger.info("start");
-
-auto bitmex_config = std::make_shared<io::swagger::client::api::ApiConfiguration>(io::swagger::client::api::ApiConfiguration{});
-
-auto api_key_id = utility::string_t{ L"ynOrYOWoC1knanjDld9RtPhC" };
-auto api_key_secret = utility::string_t{ L"0d_jDIPan7mEHSPhQDyMQJKVPJ3kEc5qbS5ed5JBWiKIsAXW" };
-auto base_url = utility::string_t{ L"https://www.bitmex.com/api/v1" };
-auto http_config = web::http::client::http_client_config{};
-auto user_agent = utility::string_t{ L"abc" };
-
-bitmex_config->setApiKey(api_key_id, api_key_secret);
-bitmex_config->setBaseUrl(base_url);
-bitmex_config->setHttpConfig(http_config);
-bitmex_config->setUserAgent(user_agent);
-
-auto bitmex_api = std::make_shared< io::swagger::client::api::ApiClient>(bitmex_config);
-
-auto trade_api = io::swagger::client::api::TradeApi{ bitmex_api };
-
-const auto symbol = utility::string_t{ L"XBTUSD" };
-const auto count = 1000;
-const auto start_time = utility::datetime::from_string(L"2020-05-24T00:00:00Z", utility::datetime::date_format::ISO_8601);
-const auto end_time = utility::datetime::from_string(L"2020-05-25T00:00:00Z", utility::datetime::date_format::ISO_8601);
-auto trade = trade_api.trade_get(
-    symbol,
-    boost::none,
-    boost::none,
-    count,
-    boost::none,
-    boost::none,
-    start_time,
-    end_time
-);
-
-auto results = trade.get();
-
-for (auto result : results) {
-    std::wcout <<
-        "Result: " << result->getTimestamp().to_string(utility::datetime::date_format::ISO_8601).c_str() <<
-        "  " << result->getPrice() <<
-        "  " << result->getSize() <<
-        std::endl;
-    break;
-}
-
-std::cout << "Count: " << results.size() << std::endl;
-
-return 1;
-*/
-
-/*
-void BitmexInterim::download_done_callback(sptr_download_data_t payload)
-{
-    auto compressed = boost::iostreams::array_source{ payload->data(), payload->size() };
-    auto out = boost::iostreams::filtering_streambuf<boost::iostreams::input>{};
-    out.push(boost::iostreams::gzip_decompressor{});
-    out.push(compressed);
-
-    auto decompressed = std::stringstream{};
-    boost::iostreams::copy(out, decompressed);
-    auto tick_data = parse_raw(decompressed);
-    //(*tick_data)[symbol]->rows.push_back({ timestamp, price, volume, buy });
-
-    if (!tick_data) {
-        logger.error("BitmexDaily::download_done_callback parsing error!");
-        shutdown();
-        return;
-    }
-
-    {
-        auto slock = std::scoped_lock{ tick_data_mutex };
-        tick_data_queue.push_back(std::move(tick_data));
-    }
-    tick_data_condition.notify_one();
-
-    start_next_download();
-}
-
-void BitmexDaily::start_next_download(void)
-{
-}
-*/
