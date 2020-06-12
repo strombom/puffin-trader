1444a8d8565c3424f5b644912e39afd68e2334e6
Johan Strömbom <strombom@gmail.com>
2020-06-12 23:24:12 +0200

    HttpSession

--- BitMin/BitMin/HttpServer.cpp
+++ BitMin/BitMin/HttpServer.cpp
@@ -91,11 +91,7 @@ void HttpListener::run(void)
 void HttpListener::do_accept(void)
 {
     // The new connection gets its own strand
-    acceptor.async_accept(
-        boost::asio::make_strand(*ioc),
-        boost::beast::bind_front_handler(
-            &HttpListener::on_accept,
-            shared_from_this()));
+    acceptor.async_accept(boost::asio::make_strand(*ioc), boost::beast::bind_front_handler(&HttpListener::on_accept, shared_from_this()));
 }
 
 void HttpListener::on_accept(boost::beast::error_code ec, boost::asio::ip::tcp::socket socket)
@@ -107,14 +103,110 @@ void HttpListener::on_accept(boost::beast::error_code ec, boost::asio::ip::tcp::
     else
     {
         // Create the session and run it
-        /*
-        std::make_shared<session>(
+        std::make_shared<HttpSession>(
             std::move(socket),
-            ctx_,
-            doc_root_)->run();
-        */
+            ctx)->run();
     }
 
     // Accept another connection
     do_accept();
+}
+
+
+HttpSession::HttpSession(boost::asio::ip::tcp::socket&& socket, std::shared_ptr<boost::asio::ssl::context> ctx) :
+    stream(std::move(socket), *ctx), lambda(*this)
+{
+
+}
+
+void HttpSession::run(void)
+{
+    // We need to be executing within a strand to perform async operations
+    // on the I/O objects in this session. Although not strictly necessary
+    // for single-threaded contexts, this example code is written to be
+    // thread-safe by default.
+    boost::asio::dispatch(stream.get_executor(), boost::beast::bind_front_handler(&HttpSession::on_run, shared_from_this()));
+}
+
+void HttpSession::on_run(void)
+{
+    // Set the timeout.
+    boost::beast::get_lowest_layer(stream).expires_after(30s);
+
+    // Perform the SSL handshake
+    stream.async_handshake(boost::asio::ssl::stream_base::server, boost::beast::bind_front_handler(&HttpSession::on_handshake, shared_from_this()));
+}
+
+void HttpSession::on_handshake(boost::beast::error_code ec)
+{
+    if (ec)
+        return fail(ec, "handshake");
+
+    do_read();
+}
+
+void HttpSession::do_read(void)
+{
+    // Make the request empty before reading,
+    // otherwise the operation behavior is undefined.
+    req = {};
+
+    // Set the timeout.
+    boost::beast::get_lowest_layer(stream).expires_after(30s);
+
+    // Read a request
+    boost::beast::http::async_read(stream, buffer, req, boost::beast::bind_front_handler(&HttpSession::on_read, shared_from_this()));
+}
+
+void HttpSession::on_read(boost::beast::error_code ec, std::size_t bytes_transferred)
+{
+    boost::ignore_unused(bytes_transferred);
+
+    // This means they closed the connection
+    if (ec == boost::beast::http::error::end_of_stream)
+        return do_close();
+
+    if (ec)
+        return fail(ec, "read");
+
+    // Send the response
+    //handle_request(std::move(req), lambda);
+}
+
+void HttpSession::on_write(bool close, boost::beast::error_code ec, std::size_t bytes_transferred)
+{
+    boost::ignore_unused(bytes_transferred);
+
+    if (ec)
+        return fail(ec, "write");
+
+    if (close)
+    {
+        // This means we should close the connection, usually because
+        // the response indicated the "Connection: close" semantic.
+        return do_close();
+    }
+
+    // We're done with the response so delete it
+    res = nullptr;
+
+    // Read another request
+    do_read();
+}
+
+void HttpSession::do_close(void)
+{
+    // Set the timeout.
+    boost::beast::get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
+
+    // Perform the SSL shutdown
+    stream.async_shutdown(boost::beast::bind_front_handler(&HttpSession::on_shutdown, shared_from_this()));
+}
+
+void HttpSession::on_shutdown(boost::beast::error_code ec)
+{
+    if (ec)
+        return fail(ec, "shutdown");
+
+    // At this point the connection is closed gracefully
 }
--- BitMin/BitMin/HttpServer.h
+++ BitMin/BitMin/HttpServer.h
@@ -6,19 +6,61 @@
 #include <atomic>
 
 
+class HttpSession : public std::enable_shared_from_this<HttpSession>
+{
+public:
+    explicit HttpSession(boost::asio::ip::tcp::socket&& socket, std::shared_ptr<boost::asio::ssl::context> ctx);
+
+    void run(void);
+
+    void on_run(void);
+    void on_handshake(boost::beast::error_code ec);
+    void do_read(void);
+    void on_read(boost::beast::error_code ec, std::size_t bytes_transferred);
+    void on_write(bool close, boost::beast::error_code ec, std::size_t bytes_transferred);
+    void do_close(void);
+    void on_shutdown(boost::beast::error_code ec);
+
+private:
+    struct send_lambda
+    {
+        HttpSession& self;
+
+        explicit send_lambda(HttpSession& self) : self(self) {}
+
+        template<bool isRequest, class Body, class Fields>
+        void operator()(boost::beast::http::message<isRequest, Body, Fields>&& msg) const
+        {
+            // The lifetime of the message has to extend for the duration of the async operation so we use a shared_ptr to manage it.
+            auto sp = std::make_shared<boost::beast::http::message<isRequest, Body, Fields>>(std::move(msg));
+
+            // Store a type-erased version of the shared pointer in the class to keep it alive.
+            self.res = sp;
+
+            // Write the response
+            boost::beast::http::async_write(self.stream, *sp, boost::beast::bind_front_handler(&HttpSession::on_write, self.shared_from_this(), sp->need_eof()));
+        }
+    };
+
+    boost::beast::ssl_stream<boost::beast::tcp_stream> stream;
+    boost::beast::flat_buffer buffer;
+    boost::beast::http::request<boost::beast::http::string_body> req;
+    std::shared_ptr<void> res;
+    send_lambda lambda;
+};
+
 class HttpListener : public std::enable_shared_from_this<HttpListener>
 {
 public:
     HttpListener(std::shared_ptr<boost::asio::io_context> ioc, std::shared_ptr<boost::asio::ssl::context> ctx, boost::asio::ip::tcp::endpoint endpoint);
 
+    void run(void);
+
+private:
     std::shared_ptr<boost::asio::io_context> ioc;
     std::shared_ptr<boost::asio::ssl::context> ctx;
     boost::asio::ip::tcp::acceptor acceptor;
 
-
-    void run(void);
-
-private:
     void do_accept(void);
     void on_accept(boost::beast::error_code ec, boost::asio::ip::tcp::socket socket);
 };
