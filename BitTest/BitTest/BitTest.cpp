1772ea0be39d83f680d82b4e7412fed26dff46c0
Johan Strömbom <strombom@gmail.com>
2020-06-15 10:50:06 +0200

    BitTest

---
+++ BitTest/BitTest/BitTest.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+
+#include <string>
+#include <iostream>
+
+
+int main()
+{
+    const auto task = std::string{ "plot_features" };
+
+    if (task == "plot_features") {
+        std::cout << "Plot features\n";
+    }
+}
---
+++ BitTest/BitTest/pch.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#pragma warning(push, 0)
+#pragma warning(disable: 4146)
+#include <torch/torch.h>
+#include <torch/script.h>
+#pragma warning(pop)
+
+#pragma warning(push)
+#pragma warning(disable: 4005)
+#include <zmq.hpp>
+#pragma warning(pop)
+
+#include "date/date.h"
+#include "json11/json11.hpp"
+/*
+#include <boost/beast/core.hpp>
+#include <boost/beast/http/write.hpp>
+#include <boost/beast/http/parser.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#include <boost/asio/strand.hpp>
+*/
