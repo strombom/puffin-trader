8eed32c21a2b6f432d3c10040d3a7d5e2f3a8b50
Johan Strömbom <strombom@gmail.com>
2020-06-15 11:58:25 +0200

    Cleanup

--- BitTest/BitTest/FeaturePlotter.cpp
+++ BitTest/BitTest/FeaturePlotter.cpp
@@ -11,15 +11,13 @@ FeaturePlotter::FeaturePlotter(const std::string& file_path, const std::string& 
     features = Utils::load_tensor(file_path, filename);
 }
 
-
 void FeaturePlotter::plot(const std::string& file_path, const std::string& filename)
 {
-    //generate some image
-    const auto width = 1000;
-    const auto height = 128;
-
     const auto local_features = features.cpu();
+
+    const auto width = 10000;
     const auto start_idx = local_features.size(0) - width - 1;
+    const auto height = (int) local_features.size(2);
 
     auto image = std::vector<unsigned char>{};
     image.resize(width * height * 4);
@@ -33,42 +31,8 @@ void FeaturePlotter::plot(const std::string& file_path, const std::string& filen
         }
     }
 
-    unsigned error = lodepng::encode(file_path + "\\" + filename, image, width, height);
-
-    //if there's an error, display it
+    const auto error = lodepng::encode(file_path + "\\" + filename, image, width, height);
     if (error) {
         std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
     }
 }
-
-
-/*
-//generate some image
-const auto width = 4000;
-const auto height = 128;
-auto image = std::vector<unsigned char>{};
-image.resize(width* height * 4);
-auto tensor = Utils::load_tensor(BitSim::tmp_path, "features.tensor").cpu();
-for (auto y = 0; y < height; y++) {
-    for (auto x = 0; x < width; x++) {
-        auto c = (unsigned int)(tensor[2108033 - width + x][0][y].item().toDouble() * 255);
-
-        image[4 * width * y + 4 * x + 0] = c;
-        image[4 * width * y + 4 * x + 1] = c;
-        image[4 * width * y + 4 * x + 2] = c;
-        image[4 * width * y + 4 * x + 3] = c;
-    }
-}
-std::cout << "Tensor " << tensor.sizes() << std::endl;
-
-const auto filename = "C:\\development\\github\\puffin-trader\\tmp\\features2.png";
-//std::vector<unsigned char> image;
-
-unsigned error = lodepng::encode(filename, image, width, height);
-
-//if there's an error, display it
-if (error) std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
-
-return 0;
-
-*/
