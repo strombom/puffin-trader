967b529ffa5dd87f6c482cbd6c771056a802f7a3
Johan Strombom <strombom@gmail.com>
2020-06-21 12:55:38 +0200

    BitTest feature plotter height

--- BitTest/BitTest/FeaturePlotter.cpp
+++ BitTest/BitTest/FeaturePlotter.cpp
@@ -13,17 +13,24 @@ FeaturePlotter::FeaturePlotter(const std::string& file_path, const std::string& 
 
 void FeaturePlotter::plot(std::vector<double> prices, const std::string& file_path, const std::string& filename)
 {
-    const auto local_features = features.cpu();
+    const auto features_height = features.size(2);
+    const auto local_features = features.argmax(2).cpu();
 
+    //const auto s = local_features.narrow(0, 15000, 25); // .argmax(1);
+    //const auto m = local_features.max();
+
+    //std::cout << s.argmax(2) << std::endl;
+
+    //return;
 
     assert(prices.size() == img_width);
 
-    const auto price_height = 256;
+    const auto price_height = 512;
     const auto price_max = *std::max_element(prices.begin(), prices.end());
     const auto price_min = *std::min_element(prices.begin(), prices.end());
 
     const auto features_start_idx = 259200; // local_features.size(0) - width - 1;
-    const auto features_height = (int) local_features.size(2);
+    //const auto features_height = (int) local_features.size(2);
 
     const auto img_width = 17280;// 10000;
     const auto img_height = price_height + features_height;
@@ -32,15 +39,18 @@ void FeaturePlotter::plot(std::vector<double> prices, const std::string& file_pa
     image.resize(img_width * img_height * 4);
     std::fill(image.begin(), image.end(), 0);
 
-    for (auto y = 0; y < features_height; y++) {
+    //for (auto y = 0; y < features_height; y++) {
         for (auto x = 0; x < img_width; x++) {
-            const auto c = (unsigned int)(local_features[features_start_idx + x][0][y].item().toDouble() * 255);
+            const auto y = local_features[features_start_idx + x][0].item().toInt();
+            const auto c = 255;
+
+            //const auto c = (unsigned int)(local_features[features_start_idx + x][0][y].item().toDouble() * 255);
             image[4 * img_width * y + 4 * x + 0] = c;
             image[4 * img_width * y + 4 * x + 1] = c;
             image[4 * img_width * y + 4 * x + 2] = c;
             image[4 * img_width * y + 4 * x + 3] = 255;
         }
-    }
+    //}
 
     for (auto x = 0; x < img_width; x++) {
         for (auto y = 0; y < price_height; y+=10) {
