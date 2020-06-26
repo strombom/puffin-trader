8b02acb74afa844ce9fa30ecfbbd388063d4a672
Johan Strombom <strombom@gmail.com>
2020-06-26 19:12:18 +0200

    BitMin front end

---
+++ BitMin/front_end/components/PlotDirection.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div>
+    <highcharts :constructor-type="'chart'" :options="chartOptions"></highcharts>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartOptions: {
+        title: {
+          text: "Direction"
+        },
+        series: [
+          {
+            data: [1, 2, 3, 2, 3, 1]
+          },
+          {
+            data: [1.5, 2.6, 3.7, 2.8, 3.9, 2]
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+
+<style>
+.red {
+  color: red;
+}
+</style>
---
+++ BitMin/front_end/plugins/highcharts.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import Highcharts from "highcharts";
+import HighchartsVue from "highcharts-vue";
+
+Vue.use(HighchartsVue, {
+	highcharts: Highcharts
+})
