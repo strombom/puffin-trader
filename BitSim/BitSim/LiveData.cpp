7abde360b90f38eb052e9d18c5221ba9cb53695a
Johan Strombom <strombom@gmail.com>
2020-09-30 19:23:23 +0200

    LiveData from delta server

--- BitSim/BitSim/LiveData.cpp
+++ BitSim/BitSim/LiveData.cpp
@@ -3,27 +3,59 @@
 #include "BitLib/Logger.h"
 #include "LiveData.h"
 
+#include <msgpack.hpp>
+
+
+struct BitmexMessageTick
+{
+public:
+    unsigned long long timestamp_ms;
+    float price;
+    float volume;
+    bool buy;
+
+    time_point_ms timestamp(void)
+    {
+        return std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds>{ std::chrono::milliseconds{ timestamp_ms } };
+    }
+
+    MSGPACK_DEFINE(timestamp_ms, price, volume, buy);
+};
 
 LiveData::LiveData(time_point_ms start_time) :
-    live_data_thread_running(true),
+    tick_data_thread_running(true),
     next_timestamp(start_time),
     agg_tick_idx(0)
 {
+    zmq_client = std::make_unique<zmq::socket_t>(zmq_context, zmq::socket_type::req);
+    zmq_client->setsockopt(ZMQ_RCVTIMEO, 2500);
+    zmq_client->setsockopt(ZMQ_SNDTIMEO, 2500);
+    zmq_client->connect(BitBase::Bitmex::Live::address);
 
+    tick_data_worker_thread = std::make_unique<std::thread>(&LiveData::tick_data_worker, this);
 }
 
 void LiveData::start(void)
 {
-    live_data_thread = std::make_unique<std::thread>(&LiveData::live_data_worker, this);
+    //live_data_thread = std::make_unique<std::thread>(&LiveData::live_data_worker, this);
+
+    tick_data_condition.notify_one();
 }
 
 void LiveData::shutdown(void)
 {
     std::cout << "LiveData: Shutting down" << std::endl;
-    live_data_thread_running = false;
+    //live_data_thread_running = false;
+    tick_data_thread_running = false;
+    tick_data_condition.notify_all();
+
+    //try {
+    //    live_data_thread->join();
+    //}
+    //catch (...) {}
 
     try {
-        live_data_thread->join();
+        tick_data_worker_thread->join();
     }
     catch (...) {}
 }
@@ -38,6 +70,85 @@ sptrAggTick LiveData::get_next_agg_tick(void)
     return nullptr;
 }
 
+void LiveData::tick_data_worker(void)
+{
+    while (tick_data_thread_running) {
+        {
+            auto tick_data_lock = std::unique_lock<std::mutex>{ tick_data_mutex };
+            tick_data_condition.wait(tick_data_lock);
+        }
+
+        while (tick_data_thread_running) {
+            for (auto&& symbol : BitBase::Bitmex::symbols) {
+
+                next_timestamp += 1ms; // Do not include the latest timestamp, only newer should be fetched
+
+                json11::Json command = json11::Json::object{
+                    { "command", "get_ticks" },
+                    { "symbol", symbol },
+                    { "max_rows", BitBase::Bitmex::Live::max_rows },
+                    { "timestamp_start", DateTime::to_string_iso_8601(next_timestamp) }
+                };
+
+                auto message = zmq::message_t{ command.dump() };
+                auto result = zmq::send_result_t{};
+                try {
+                    result = zmq_client->send(message, zmq::send_flags::dontwait);
+                }
+                catch (...) {
+                    result.reset();
+                }
+
+                if (result.has_value()) {
+                    auto result = zmq_client->recv(message);
+
+                    if (result.has_value()) {
+                        auto received_ticks = std::vector<BitmexMessageTick>{};
+                        received_ticks = msgpack::unpack(static_cast<const char*>(message.data()), message.size()).get().convert(received_ticks);
+
+                        auto last_timepoint = std::chrono::system_clock::time_point(std::chrono::system_clock::now() - 500ms);
+                        if (received_ticks.size() > 1) {
+                            if (received_ticks.back().timestamp() < last_timepoint) {
+                                last_timepoint = received_ticks.back().timestamp();
+                            }
+                            else {
+                                last_timepoint = std::chrono::system_clock::time_point(std::chrono::system_clock::now());
+                            }
+                        }
+                        auto last_timestamp = std::chrono::time_point_cast<std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds>::duration>(last_timepoint);
+
+                        auto tick_data = std::make_unique<Ticks>();
+                        for (auto& tick : received_ticks) {
+                            if (tick.timestamp() < last_timestamp) {
+                                tick_data->rows.push_back({ tick.timestamp(), tick.price, tick.volume, tick.buy });
+
+                                auto lock = std::scoped_lock{ agg_ticks_mutex };
+                                agg_ticks.insert(Tick{ tick.timestamp(), tick.price, tick.volume, tick.buy });
+                                next_timestamp = tick.timestamp();
+
+                            }
+                        }
+
+                        if (tick_data->rows.size() > 0) {
+                            logger.info("BitmexLive::tick_data_worker append count(%d) (%s) (%0.1f)", (int)tick_data->rows.size(), DateTime::to_string(last_timestamp).c_str(), tick_data->rows.back().price);
+                        }
+                    }
+                    else {
+                        logger.info("BitmexLive::tick_data_worker zmq recv fail");
+                    }
+                }
+                else {
+                    logger.info("BitmexLive::tick_data_worker zmq send fail");
+                }
+
+                std::this_thread::sleep_for(10ms);
+            }
+        }
+    }
+    logger.info("BitmexDaily::tick_data_worker exit");
+}
+
+/*
 void LiveData::live_data_worker(void)
 {
     while (live_data_thread_running) {
@@ -50,3 +161,4 @@ void LiveData::live_data_worker(void)
         }
     }
 }
+*/
--- BitSim/BitSim/LiveData.h
+++ BitSim/BitSim/LiveData.h
@@ -19,17 +19,25 @@ public:
     sptrAggTick get_next_agg_tick(void);
 
 private:
-    BitBaseClient bitbase_client;
+    std::mutex tick_data_mutex;
+    std::unique_ptr<std::thread> tick_data_worker_thread;
+    std::condition_variable tick_data_condition;
+    std::atomic_bool tick_data_thread_running;
+
+    zmq::context_t zmq_context;
+    std::unique_ptr<zmq::socket_t> zmq_client;
 
     time_point_ms next_timestamp;
     std::mutex agg_ticks_mutex;
     AggTicks agg_ticks;
     size_t agg_tick_idx;
 
-    std::atomic_bool live_data_thread_running;
-    std::unique_ptr<std::thread> live_data_thread;
+    void tick_data_worker(void);
 
-    void live_data_worker(void);
+    //BitBaseClient bitbase_client;
+    //std::atomic_bool live_data_thread_running;
+    //std::unique_ptr<std::thread> live_data_thread;
+    //void live_data_worker(void);
 };
 
 using sptrLiveData = std::shared_ptr<LiveData>;
