85600d231ff534018687577d00fd6107a8792bfb
Johan Strömbom <strombom@gmail.com>
2020-10-07 12:57:37 +0200

    Limit orders

--- BitKin/Common/trading.py
+++ BitKin/Common/trading.py
@@ -1,26 +1,11 @@
 
 
 class Position:
-    def __init__(self, mark_price, initial_leverage, take_profit, min_profit):
+    def __init__(self, settings, mark_price, initial_leverage=0):
+        self.settings = settings
         self.wallet = 1.0
         self.price = mark_price
-        self.contracts = 0 #initial_leverage * self.wallet * self.price
-        self.take_profit = take_profit
-        self.min_profit = min_profit
-        self._update()
-
-    def _update(self):
-        if self.contracts == 0:
-            self.direction = 1
-        else:
-            self.direction = self.contracts / abs(self.contracts)
-        self.take_profit_price = self.price * (1 + self.direction * take_profit)
-        self.stop_loss_price = self.price * (1 - self.direction * stop_loss)
-
-        if self.wallet < 0.2:
-            self.wallet = 0
-            self.contracts = 0
-            return
+        self.contracts = initial_leverage * self.wallet * self.price
 
     def get_leverage(self, mark_price):
         if self.wallet == 0:
@@ -35,38 +20,54 @@ class Position:
         upnl = self.contracts * (1 / self.price - 1 / mark_price)
         return (self.wallet + upnl) * mark_price
 
-    def market_order(self, order_leverage, mark_price, timestamp):
+    def calculate_order_size(self, leverage, mark_price):
+        if self.wallet == 0.0:
+            return 0.0
 
+        position_margin = 0.0
+        position_leverage = 0.0
+        upnl = 0.0
+
+        if self.contracts != 0.0:
+            sign = self.contracts / abs(self.contracts)
+            entry_value = abs(self.contracts / self.price)
+            mark_value = abs(self.contracts / mark_price)
+            upnl = sign * (entry_value - mark_value)
+            position_margin = max(0.0, abs(self.contracts / self.price) - upnl)
+            position_leverage = sign * position_margin / self.wallet
+
+        max_contracts = self.settings['max_leverage'] * (self.wallet + upnl) * mark_price
+        margin = self.wallet * min(max(leverage, -self.settings['max_leverage']), self.settings['max_leverage'])
+        contracts = min(max(margin * mark_price, -max_contracts), max_contracts)
+
+        order_contracts = contracts - self.contracts
+        return order_contracts
+
+    def limit_order(self, order_contracts, mark_price):
         # print('market_order', wallet, pos_price, pos_contracts, order_contracts, mark_price)
         if self.wallet == 0:
             return
 
         upnl = self.contracts * (1 / self.price - 1 / mark_price)
 
-        #if timestamp > 1578035300:
-        #    print("a")
-        #elif timestamp > 1578416400:
-        #    print("a")
-
-        max_contracts = settings['max_leverage'] * (self.wallet + upnl) * mark_price
-        max_contracts = min(max_contracts, settings['max_order_value'] * mark_price)
-
-        margin = self.wallet * min(max(order_leverage, -settings['max_leverage']), settings['max_leverage'])
-        order_contracts = min(max(margin * mark_price, -max_contracts), max_contracts) - self.contracts
+        max_contracts = self.settings['max_leverage'] * (self.wallet + upnl) * mark_price
+        if self.contracts + order_contracts > max_contracts or self.contracts + order_contracts < -max_contracts:
+            order_contracts = max_contracts - self.contracts
 
         # Fee
-        fee = taker_fee * abs(order_contracts) / mark_price
+        maker_fee = -0.0025
+        fee = maker_fee * abs(order_contracts) / mark_price
         self.wallet -= fee
 
         # Realised profit and loss
         # Wallet only changes when abs(contracts) decrease
-        realised = self.wallet
+        #realised = self.wallet
         if (self.contracts > 0) and (order_contracts < 0):
             self.wallet += (1 / self.price - 1 / mark_price) * min(-order_contracts, self.contracts)
         elif (self.contracts < 0) and (order_contracts > 0):
             self.wallet += (1 / self.price - 1 / mark_price) * max(-order_contracts, self.contracts)
 
-        realised = self.wallet - realised
+        #realised = self.wallet - realised
         #if realised != 0:
         #    print("realise", self.wallet, realised, fee, realised - fee)
 
@@ -80,4 +81,3 @@ class Position:
 
         # Calculate position contracts
         self.contracts += order_contracts
-        self._update()
