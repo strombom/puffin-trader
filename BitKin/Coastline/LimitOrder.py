27256ad2d971eaca6ab93a138909da71b74cd861
Johan Strombom <strombom@gmail.com>
2020-10-10 22:08:29 +0200

    Round orders to 0.5

--- BitKin/Coastline/LimitOrder.py
+++ BitKin/Coastline/LimitOrder.py
@@ -3,9 +3,10 @@
 class LimitOrder:
     def __init__(self, side, price, volume, event_type):
         self.side = side
-        self.price = price
+        self.price = round(price * 2) / 2
         self.volume = volume
         self.event_type = event_type
+        print(f'New order side({side}) price({self.price}) vol({volume}) event({event_type})')
 
     def balance_orders(self, orders):
         pass
