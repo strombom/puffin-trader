cb927ecccd46b2e5651e810852356222c6e925ad
Johan Strombom <strombom@gmail.com>
2020-10-10 10:12:49 +0200

    Make OrderBooks

--- BitKin/Coastline/OrderBook.py
+++ BitKin/Coastline/OrderBook.py
@@ -14,7 +14,13 @@ def make_order_books(agg_ticks):
     order_books = []
     ask, bid = agg_ticks[0].high, agg_ticks[0].low
     for agg_tick in agg_ticks:
-        order_book = OrderBook(ask=ask, bid=bid)
-        order_books.append(order_book)
-
+        if agg_tick.high > ask:
+            ask = agg_tick.high
+        elif agg_tick.high < ask:
+            ask = agg_tick.high + 0.5
+        if agg_tick.low < bid:
+            bid = agg_tick.low
+        elif agg_tick.low > bid:
+            bid = agg_tick.low - 0.5
+        order_books.append(OrderBook(ask=ask, bid=bid))
     return order_books
