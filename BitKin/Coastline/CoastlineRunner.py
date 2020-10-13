f6035ca663c329e71d1b265815075906fc89e808
Johan Strombom <strombom@gmail.com>
2020-10-13 19:47:10 +0200

    Bugfix, separate threshold updates

--- BitKin/Coastline/CoastlineRunner.py
+++ BitKin/Coastline/CoastlineRunner.py
@@ -20,7 +20,8 @@ class CoastlineRunner:
         if not self.initialized:
             self.extreme_price = mark_price.mid
             self.reference_price = mark_price.mid
-            self._update_thresholds()
+            self._update_direction_change_threshold()
+            self._update_overshoot_threshold()
             self.initialized = True
 
         if self.direction == Direction.up:
@@ -28,15 +29,16 @@ class CoastlineRunner:
                 self.direction = Direction.down
                 self.extreme_price = mark_price.ask
                 self.reference_price = mark_price.ask
-                self._update_thresholds()
+                self._update_direction_change_threshold()
+                self._update_overshoot_threshold()
                 return RunnerEvent.direction_change_down
 
             if mark_price.bid > self.extreme_price:
                 self.extreme_price = mark_price.bid
-                self._update_thresholds()
+                self._update_direction_change_threshold()
                 if mark_price.bid > self.overshoot_threshold:
                     self.reference_price = self.extreme_price
-                    self._update_thresholds()
+                    self._update_overshoot_threshold()
                     return RunnerEvent.overshoot_down
 
         elif self.direction == Direction.down:
@@ -44,27 +46,32 @@ class CoastlineRunner:
                 self.direction = Direction.up
                 self.extreme_price = mark_price.bid
                 self.reference_price = mark_price.bid
-                self._update_thresholds()
+                self._update_direction_change_threshold()
+                self._update_overshoot_threshold()
                 return RunnerEvent.direction_change_up
 
             if mark_price.ask < self.extreme_price:
                 self.extreme_price = mark_price.ask
-                self._update_thresholds()
+                self._update_direction_change_threshold()
                 if mark_price.ask < self.overshoot_threshold:
                     self.reference_price = mark_price.ask
                     self.extreme_price = mark_price.ask
-                    self._update_thresholds()
+                    self._update_overshoot_threshold()
                     return RunnerEvent.overshoot_up
 
         return RunnerEvent.nothing
 
-    def _update_thresholds(self):
+    def _update_direction_change_threshold(self):
         if self.direction == Direction.up:
-            self.overshoot_threshold = math.exp(math.log(self.extreme_price) + self.delta_star_up)
             self.direction_change_threshold = math.exp(math.log(self.reference_price) - self.delta_down)
         else:
-            self.overshoot_threshold = math.exp(math.log(self.extreme_price) - self.delta_star_down)
             self.direction_change_threshold = math.exp(math.log(self.reference_price) + self.delta_up)
+
+    def _update_overshoot_threshold(self):
+        if self.direction == Direction.up:
+            self.overshoot_threshold = math.exp(math.log(self.extreme_price) + self.delta_star_up)
+        else:
+            self.overshoot_threshold = math.exp(math.log(self.extreme_price) - self.delta_star_down)
 
     def get_upper_event_type(self):
         if self.direction_change_threshold > self.overshoot_threshold:
