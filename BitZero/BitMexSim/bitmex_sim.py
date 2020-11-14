c1741eacbee2e4fbbadf0669c01e9f6e66c664d9
Johan Strombom <strombom@gmail.com>
2020-11-14 16:37:38 +0100

    Environment

---
+++ BitZero/BitMexSim/bitmex_sim.py
@@ -0,0 +1,18 @@
+
+class BitMexSim:
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        observation, reward, done = [], 0.0, True
+        return observation, reward, done
+
+    def reset(self):
+        observation = []
+        return observation
+
+    def render(self):
+        pass
+
+    def close(self):
+        pass
--- BitZero/MuZero/games/bitzero.py
+++ BitZero/MuZero/games/bitzero.py
@@ -1,16 +1,125 @@
-from abc import ABC, abstractmethod
+import os
+
+import numpy as np
+import torch
+import datetime
+
+from BitMexSim.bitmex_sim import BitMexSim
+from MuZero.games.abstract_game import AbstractGame
 
 
-class BitZero(ABC):
+class MuZeroConfig:
+    def __init__(self):
+        # More information is available here: https://github.com/werner-duvaud/muzero-general/wiki/Hyperparameter-Optimization
+
+        self.seed = 0  # Seed for numpy, torch and the game
+        self.max_num_gpus = 1  # Fix the maximum number of GPUs to use. It's usually faster to use a single GPU (set it to 1) if it has enough memory. None will use every GPUs available
+
+        ### Game
+        self.observation_shape = (1, 1, 8)  # Dimensions of the game observation, must be 3D (channel, height, width). For a 1D array, please reshape it to (1, 1, length of array)
+        self.action_space = list(range(3))  # Fixed list of all possible actions. You should only edit the length
+        self.players = list(range(1))  # List of players. You should only edit the length
+        self.stacked_observations = 0  # Number of previous observations and previous actions to add to the current observation
+
+        # Evaluate
+        self.muzero_player = 0  # Turn Muzero begins to play (0: MuZero plays first, 1: MuZero plays second)
+        self.opponent = None  # Hard coded agent that MuZero faces to assess his progress in multiplayer games. It doesn't influence training. None, "random" or "expert" if implemented in the Game class
+
+        ### Self-Play
+        self.num_workers = 1  # Number of simultaneous threads/workers self-playing to feed the replay buffer
+        self.selfplay_on_gpu = False
+        self.max_moves = 700  # Maximum number of moves if game is not finished before
+        self.num_simulations = 50  # Number of future moves self-simulated
+        self.discount = 0.999  # Chronological discount of the reward
+        self.temperature_threshold = None  # Number of moves before dropping the temperature given by visit_softmax_temperature_fn to 0 (ie selecting the best action). If None, visit_softmax_temperature_fn is used every time
+
+        # Root prior exploration noise
+        self.root_dirichlet_alpha = 0.25
+        self.root_exploration_fraction = 0.25
+
+        # UCB formula
+        self.pb_c_base = 19652
+        self.pb_c_init = 1.25
+
+        ### Network
+        self.network = "fullyconnected"  # "resnet" / "fullyconnected"
+        self.support_size = 10  # Value and reward are scaled (with almost sqrt) and encoded on a vector with a range of -support_size to support_size. Choose it so that support_size <= sqrt(max(abs(discounted reward)))
+
+        # Residual Network
+        self.downsample = False  # Downsample observations before representation network, False / "CNN" (lighter) / "resnet" (See paper appendix Network Architecture)
+        self.blocks = 2  # Number of blocks in the ResNet
+        self.channels = 16  # Number of channels in the ResNet
+        self.reduced_channels_reward = 16  # Number of channels in reward head
+        self.reduced_channels_value = 16  # Number of channels in value head
+        self.reduced_channels_policy = 16  # Number of channels in policy head
+        self.resnet_fc_reward_layers = []  # Define the hidden layers in the reward head of the dynamic network
+        self.resnet_fc_value_layers = []  # Define the hidden layers in the value head of the prediction network
+        self.resnet_fc_policy_layers = []  # Define the hidden layers in the policy head of the prediction network
+
+        # Fully Connected Network
+        self.encoding_size = 10
+        self.fc_representation_layers = []  # Define the hidden layers in the representation network
+        self.fc_dynamics_layers = [64]  # Define the hidden layers in the dynamics network
+        self.fc_reward_layers = [64]  # Define the hidden layers in the reward network
+        self.fc_value_layers = [64]  # Define the hidden layers in the value network
+        self.fc_policy_layers = [64]  # Define the hidden layers in the policy network
+
+        ### Training
+        self.results_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../results",
+                                         os.path.basename(__file__)[:-3], datetime.datetime.now().strftime(
+                "%Y-%m-%d--%H-%M-%S"))  # Path to store the model weights and TensorBoard logs
+        self.save_model = True  # Save the checkpoint in results_path as model.checkpoint
+        self.training_steps = 200000  # Total number of training steps (ie weights update according to a batch)
+        self.batch_size = 64  # Number of parts of games to train on at each training step
+        self.checkpoint_interval = 10  # Number of training steps before using the model for self-playing
+        self.value_loss_weight = 1  # Scale the value loss to avoid overfitting of the value function, paper recommends 0.25 (See paper appendix Reanalyze)
+        self.train_on_gpu = True if torch.cuda.is_available() else False  # Train on GPU if available
+
+        self.optimizer = "Adam"  # "Adam" or "SGD". Paper uses SGD
+        self.weight_decay = 1e-4  # L2 weights regularization
+        self.momentum = 0.9  # Used only if optimizer is SGD
+
+        # Exponential learning rate schedule
+        self.lr_init = 0.005  # Initial learning rate
+        self.lr_decay_rate = 1  # Set it to 1 to use a constant learning rate
+        self.lr_decay_steps = 1000
+
+        ### Replay Buffer
+        self.replay_buffer_size = 2000  # Number of self-play games to keep in the replay buffer
+        self.num_unroll_steps = 10  # Number of game moves to keep for every batch element
+        self.td_steps = 30  # Number of steps in the future to take into account for calculating the target value
+        self.PER = True  # Prioritized Replay (See paper appendix Training), select in priority the elements in the replay buffer which are unexpected for the network
+        self.PER_alpha = 0.5  # How much prioritization is used, 0 corresponding to the uniform case, paper suggests 1
+
+        # Reanalyze (See paper appendix Reanalyse)
+        self.use_last_model_value = True  # Use the last model to provide a fresher, stable n-step value (See paper appendix Reanalyze)
+        self.reanalyse_on_gpu = False
+
+        # Best known ratio for deterministic version: 0.8 --> 0.4 in 250 self played game (self_play_delay = 25 on GTX 1050Ti Max-Q).
+        ### Adjust the self play / training ratio to avoid over/underfitting
+        self.self_play_delay = 0  # Number of seconds to wait after each played game
+        self.training_delay = 0  # Number of seconds to wait after each training step
+        self.ratio = None  # Desired training steps per self played step ratio. Equivalent to a synchronous version, training can take much longer. Set it to None to disable it
+
+    def visit_softmax_temperature_fn(self, trained_steps):
+        """
+        Parameter to alter the visit count distribution to ensure that the action selection becomes greedier as training progresses.
+        The smaller it is, the more likely the best action (ie with the highest visit count) is chosen.
+
+        Returns:
+            Positive float.
+        """
+        return 0.35
+
+
+class BitZero(AbstractGame):
     """
     Inherit this class for muzero to play
     """
 
-    @abstractmethod
     def __init__(self, seed=None):
-        pass
+        self.env = BitMexSim()
 
-    @abstractmethod
     def step(self, action):
         """
         Apply action to the game.
@@ -21,18 +130,9 @@ class BitZero(ABC):
         Returns:
             The new observation, the reward and a boolean if the game has ended.
         """
-        pass
+        observation, reward, done, _ = self.env.step(action)
+        return numpy.array([[observation]]), reward, done
 
-    def to_play(self):
-        """
-        Return the current player.
-
-        Returns:
-            The current player, it should be an element of the players list in the config. 
-        """
-        return 0
-
-    @abstractmethod
     def legal_actions(self):
         """
         Should return the legal actions at each turn, if it is not available, it can return
@@ -44,9 +144,8 @@ class BitZero(ABC):
         Returns:
             An array of integers, subset of the action space.
         """
-        pass
+        return list(range(3))
 
-    @abstractmethod
     def reset(self):
         """
         Reset the game for a new game.
@@ -54,43 +153,20 @@ class BitZero(ABC):
         Returns:
             Initial observation of the game.
         """
-        pass
+        return numpy.array([[self.env.reset()]])
 
     def close(self):
         """
         Properly close the game.
         """
-        pass
+        self.env.close()
 
-    @abstractmethod
     def render(self):
         """
         Display the game observation.
         """
-        pass
-
-    def human_to_action(self):
-        """
-        For multiplayer games, ask the user for a legal action
-        and return the corresponding action number.
-
-        Returns:
-            An integer from the action space.
-        """
-        choice = input(f"Enter the action to play for the player {self.to_play()}: ")
-        while int(choice) not in self.legal_actions():
-            choice = input("Ilegal action. Enter another action : ")
-        return int(choice)
-
-    def expert_agent(self):
-        """
-        Hard coded agent that MuZero faces to assess his progress in multiplayer games.
-        It doesn't influence training
-
-        Returns:
-            Action as an integer to take in the current game state
-        """
-        raise NotImplementedError
+        self.env.render()
+        input("Press enter to take a step ")
 
     def action_to_string(self, action_number):
         """
@@ -102,4 +178,9 @@ class BitZero(ABC):
         Returns:
             String representing the action.
         """
-        return str(action_number)
+        actions = {
+            0: "Hodl",
+            1: "Market buy",
+            2: "Market sell"
+        }
+        return f"{action_number}. {actions[action_number]}"
