f16e49dbf2bc77af66bc53fe8e95d4c59ebcb75f
Johan Strombom <strombom@gmail.com>
2020-11-28 19:40:41 +0100

    Fix delta measurement

--- BitTer/measure_delta.py
+++ BitTer/measure_delta.py
@@ -13,37 +13,45 @@ if __name__ == '__main__':
     for delta_idx in range(len(deltas)):
         delta = deltas[delta_idx]
 
-        ie_os_delta_up = []
-        ie_os_delta_down = []
+        ie_os_delta_up = [] #{}
+        ie_os_delta_down = [] #{}
 
         runner = runners[0]
 
-        os_idx = 1
-        ie_price = runner.ie_prices[0]
+        ie_idx = 1
+        for os_idx in range(len(runner.os_times)):
+            count = 0
+            while ie_idx + 1 < len(runner.ie_times) and runner.ie_times[ie_idx] <= runner.os_times[os_idx]:
+                count += 1
+                ie_idx += 1
 
-        for ie_idx in range(len(runner.ie_times)):
-            if runner.ie_times[ie_idx] >= runner.os_times[os_idx]:
-                delta_price = (runner.os_prices[os_idx] - ie_price) / ie_price / delta
-                if delta_price > 20 or delta_price < -20:
-                    print(f'hm {runner.ie_times[ie_idx]}, {runner.ie_prices[ie_idx]}, {delta_price}')
+            os_price, ie_price = runner.os_prices[os_idx], runner.ie_prices[ie_idx - 1]
+            delta_price = (os_price - ie_price) / ie_price / delta
 
-                if delta_price > 0:
-                    ie_os_delta_up.append(delta_price)
-                else:
-                    ie_os_delta_down.append(-delta_price)
-                os_idx += 1
-                if os_idx == len(runner.os_times):
-                    break
-            ie_price = runner.ie_prices[ie_idx]
+            if delta_price > 0:
+                #if count not in ie_os_delta_up:
+                #    ie_os_delta_up[count] = []
+                #if count == 2:
+                #    ie_os_delta_up[1].append(delta_price + 1)
+                #else:
+                ie_os_delta_up.append(delta_price + count - 1)
+            else:
+                #if count not in ie_os_delta_down:
+                #    ie_os_delta_down[count] = []
+                ie_os_delta_down.append(delta_price - count + 1)
 
-        ie_os_delta_up = np.array(ie_os_delta_up)
-        ie_os_delta_down = np.array(ie_os_delta_down)
+        #for count in ie_os_delta_up:
+        #    print(count)
+        #    ie_os_delta_up[count] = np.array(ie_os_delta_up[count])
+        #for count in ie_os_delta_down:
+        #    print(count)
+        #    ie_os_delta_down[count] = np.array(ie_os_delta_down[count])
 
         print(ie_os_delta_up)
         print(ie_os_delta_down)
 
-        plt.hist(ie_os_delta_up, bins=50, label='up')
-        plt.hist(ie_os_delta_down, bins=50, label='down')
+        plt.hist(ie_os_delta_up, bins=450, label='up')
+        plt.hist(ie_os_delta_down, bins=450, label='down')
 
         plt.legend()
         plt.show()
