3893d313f8d507f92d4b14119a28d541bbd08c5f
Johan Strombom <strombom@gmail.com>
2021-01-19 15:06:08 +0100

    Tick formatter, plot colors

--- BitBow/puffin.py
+++ BitBow/puffin.py
@@ -1,11 +1,20 @@
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
+from enum import IntEnum
 from scipy import stats
 from datetime import datetime, timezone
 
 from Indicators.supersmoother import SuperSmoother
+from IntrinsicTime.runner import Direction
 from rainbow import rainbow_indicator_load_params, rainbow_indicator
+
+
+class PositionDirection(IntEnum):
+    long = 1
+    hedge = 0
+    short = -1
 
 
 class Simulator:
@@ -30,9 +39,7 @@ if __name__ == '__main__':
     with open(f"cache/intrinsic_time_runner.pickle", 'rb') as f:
         delta, runner = pickle.load(f)
 
-    # runner.os_prices = np.array(runner.os_prices)
-    # runner.dc_prices = np.array(runner.dc_prices)
-    runner.ie_prices = np.array(runner.ie_prices)[0:2000]
+    runner.ie_prices = np.array(runner.ie_prices)[0:1000]
     x = np.arange(runner.ie_prices.shape[0])
 
     """
@@ -58,38 +65,53 @@ if __name__ == '__main__':
     def estimate_slope(start, stop):
         c_x = np.arange(start, stop)
         r = stats.linregress(c_x, runner.ie_prices[start:stop])
-        print('slope', r.slope)
+        # print('slope', r.slope)
         return c_x, c_x * r.slope + r.intercept
 
-    """
-    slope < -10 : 
-    """
 
+    plot_events = {
+        PositionDirection.long:  {'x': [], 'y': []},
+        PositionDirection.hedge: {'x': [], 'y': []},
+        PositionDirection.short: {'x': [], 'y': []}
+    }
+
+    first_idx = 100
     simulator = Simulator()
-    direction = -1
+    direction = PositionDirection.short
     state = 'estimate_slope'
     puffin_price = runner.ie_prices[0]
     slope = None
-    slope_start_x = 0
+    slope_start_x = first_idx - 20
     threshold = puffin_price * (1 + 2 * delta)
     thresholds = {'x': [], 'y': []}
-    # directions = {'x': [], 'y': []}
     slopes = []
     values = []
-    for idx, ie_price in enumerate(runner.ie_prices):
-        values.append(simulator.get_value(ie_price))
-        if idx < 10:
-            thresholds['x'].append(idx)
-            thresholds['y'].append(ie_price)
-            continue
 
+
+    def find_best_fit(idx: int):
+        pass
+
+
+    find_best_fit(first_idx)
+
+    for idx, ie_price in enumerate(runner.ie_prices[:first_idx]):
+        values.append(simulator.get_value(ie_price))
+        plot_events[PositionDirection.hedge]['x'].append(idx)
+        plot_events[PositionDirection.hedge]['y'].append(ie_price)
+
+    for idx in range(first_idx, runner.ie_prices.shape[0]):
+        ie_price = runner.ie_prices[idx]
+        values.append(simulator.get_value(ie_price))
         slope_start_x = max(slope_start_x, idx - 20)
 
         if idx - slope_start_x >= 10:
             slope_x, slope_y = estimate_slope(slope_start_x, idx)
-            slopes.append({'x': slope_x, 'y': slope_y})
+            # slopes.append({'x': slope_x, 'y': slope_y})
 
-        if direction == -1:
+        plot_events[direction]['x'].append(idx)
+        plot_events[direction]['y'].append(ie_price)
+
+        if direction == PositionDirection.short:
             threshold = slope_y[-1] * (1 + 2 * delta)
             thresholds['x'].append(idx)
             thresholds['y'].append(threshold)
@@ -100,7 +122,7 @@ if __name__ == '__main__':
                 # state = 'estimate_slope'
                 slope_start_x = idx
 
-        else:
+        elif direction == PositionDirection.long:
             threshold = slope_y[-1] * (1 - 2 * delta)
             thresholds['x'].append(idx)
             thresholds['y'].append(threshold)
@@ -111,39 +133,48 @@ if __name__ == '__main__':
                 # state = 'estimate_slope'
                 slope_start_x = idx
 
-    p = np.array(runner.ie_prices)
-    f, (ax1, ax2) = plt.subplots(2, 1, sharex='all', gridspec_kw={'height_ratios': [3, 1]})
-    # ax1 = plt.subplot(2, 1, 1)
-    # plt.plot(times, prices, label=f'price')
-    # plt.plot(times, asks, label=f'ask')
-    # plt.plot(times, bids, label=f'bid')
-    # plt.plot(runner.os_times, runner.os_prices, label=f'OS')
+    f, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1,
+                                      sharex='col',
+                                      gridspec_kw={'height_ratios': [4, 1, 1]},
+                                      figsize=(10, 9))
+    ax1.grid(which='minor', alpha=0.3)
+    ax1.grid(which='major', alpha=0.3)
+    ax1.set_yscale('log', base=10)
+    ax1.set_xlim([first_idx - 50, first_idx + 200])
+    fmt = FormatStrFormatter('%.0f')
+    ax1.yaxis.set_major_formatter(fmt)
+    ax1.yaxis.set_minor_formatter(fmt)
+
+    ax1.scatter(thresholds['x'], thresholds['y'], marker='_', c='xkcd:gold', label=f'Threshold')
+
+    for direction, color, label in ((PositionDirection.long, 'xkcd:green', 'Long'),
+                                    (PositionDirection.hedge, 'xkcd:light blue', 'Hedge'),
+                                    (PositionDirection.short, 'xkcd:red', 'Short')):
+        ax1.scatter(plot_events[direction]['x'], plot_events[direction]['y'], label=label, s=2 ** 2, c=color)
+
+    ax2.grid(True)
+    ax3.grid(True)
+
+    ax1.legend(loc='upper left')
+
+    ax3.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+
+    plt.tight_layout()
+    plt.show()
+    plt.get_current_fig_manager().toolbar.pan()
+
+    """
+    # formatter = ScalarFormatter()
+    # ax1.yaxis.set_major_formatter(formatter)
+    # ax1.ticklabel_format(axis='y', style='plain', useOffset=False)
+    # formatter.set_scientific(False)
+
     # plt.scatter(runner.os_times, runner.os_prices, label=f'OS', s=5 ** 2)
     # plt.scatter(runner.dc_times, runner.dc_prices, label=f'DC', s=7 ** 2)
     # plt.scatter(runner.ie_times, runner.ie_prices, label=f'IE', s=5 ** 2)
-
-    # plt.scatter(x, runner.dc_prices, label=f'DC', s=7 ** 2)
-    ax1.scatter(x, runner.ie_prices, label=f'IE', s=2 ** 2, c='green')
-    for smooth_period in smooths:
-        smooth = smooths[smooth_period]
-        ax1.plot(x, smooth, label=f'Smooth {smooth_period}')
 
     for slope in slopes:
         # print(slope['x'])
         # print(slope['y'])
         ax1.plot(slope['x'], slope['y'])
-
-    ax1.plot(thresholds['x'], thresholds['y'], label=f'Threshold')
-
-    ax1.plot()
-    ax1.grid(True)
-    ax1.set_yscale('log')
-
-    ax2.plot(x, values, label=f'Value')
-
-    # plt.scatter(runner.ie_times, runner.ie_min_asks, label=f'Min Asks', s=5 ** 2)
-    # plt.scatter(runner.ie_times, runner.ie_max_bids, label=f'Max Bids', s=5 ** 2)
-
-    plt.legend()
-
-    plt.show()
+    """
