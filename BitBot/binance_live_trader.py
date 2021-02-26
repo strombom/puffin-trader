b0dc9f080ed3c340faf24371c7cef94942e2f3ca
Johan Strombom <strombom@gmail.com>
2021-02-26 16:17:59 +0100

    Single thread trader

--- BitBot/binance_live_trader.py
+++ BitBot/binance_live_trader.py
@@ -1,15 +1,13 @@
-
 import zmq
 import json
-import signal
 import msgpack
-import threading
-import ratelimit
 import numpy as np
+from time import sleep
 from collections import deque
-from multiprocessing import Pipe
+from binance.client import Client
 from datetime import datetime, timedelta
 
+from binance.websockets import BinanceSocketManager
 from binance_account import BinanceAccount
 from slopes import Slopes, Slope
 from position_live import PositionLive
@@ -17,44 +15,46 @@ from Common.Misc import PositionDirection
 from IntrinsicTime.live_runner import LiveRunner
 
 
-def live_data(data_pipe: Pipe):
+def get_historic_data(runner, ie_prices):
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://192.168.1.90:31003")
 
-    def get_data(last_timestamp: datetime):
-        timestamp_start_str = last_timestamp.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z'
+    timestamp_start = datetime.utcnow() - timedelta(minutes=60)
+    while datetime.utcnow() - timestamp_start > timedelta(seconds=2):
         command = {'command': 'get_ticks',
                    'symbol': 'BTCUSDT',
                    'max_rows': 1000,
-                   'timestamp_start': timestamp_start_str}
-        # print(command)
+                   'timestamp_start': timestamp_start.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z'}
         command = json.dumps(command).encode()
         socket.send(command)
+        message = msgpack.unpackb(socket.recv())
 
-        message = socket.recv()
-        message = msgpack.unpackb(message)
-
+        historic_data = []
         for row in message:
             # print(row[0] // 1000)
             timestamp = datetime.utcfromtimestamp(row[0] / 1000)
             price = row[1]
             buy = not row[3]
             # print(timestamp, price, buy)
-            data_pipe.send((timestamp, price, buy))
-            last_timestamp = timestamp + timedelta(milliseconds=1)
+            historic_data.append((timestamp, price, buy))
+            timestamp_start = timestamp + timedelta(milliseconds=1)
 
-        return last_timestamp
-
-    timestamp_start = datetime.utcnow() - timedelta(minutes=2)
-    while True:
-        if data_pipe.poll(0.5):
-            print("live_data has msg " + data_pipe.recv())
-            data_pipe.send({'command': 'quit'})
-            break
-        # print("t start", timestamp_start)
-        timestamp_start = get_data(timestamp_start)
-        # print("t new", timestamp_start)
+        ask, bid = 0, 0
+        for timestamp, price, buy in historic_data:
+            if buy:
+                ask = price
+            else:
+                bid = price
+            if ask == 0:
+                ask = bid
+            if bid == 0:
+                bid = ask
+            runner.step(ask=ask, bid=bid)
+            new_ie_prices = runner.step(ask=ask, bid=bid)
+            if len(new_ie_prices) > 0:
+                for ie_price in new_ie_prices:
+                    ie_prices.append(ie_price)
 
 
 class LivePlotter:
@@ -68,71 +68,53 @@ class LivePlotter:
         pass
 
 
-def trader(data_pipe: Pipe):
-    binance_account = BinanceAccount()
-    from time import sleep
+def trader():
+    with open('binance_account.json') as f:
+        account_info = json.load(f)
+        api_key = account_info['api_key']
+        api_secret = account_info['api_secret']
+
+    binance_client = Client(api_key, api_secret)
+
+    delta = 0.0015
+    initial_price = 40000.0
+    runner = LiveRunner(delta=delta, initial_price=initial_price)
+
+    binance_account = BinanceAccount(binance_client)
+    position = PositionLive(delta=delta, initial_price=initial_price, direction=PositionDirection.long)
+    ie_prices = deque(maxlen=Slopes.max_slope_length + 10)
+    get_historic_data(runner, ie_prices)
+
+    def process_depth_message(data):
+        ask, bid = float(data['asks'][0][0]), float(data['bids'][0][0])
+        new_ie_prices = runner.step(ask=ask, bid=bid)
+        if len(new_ie_prices) > 0:
+            for ie_price in new_ie_prices:
+                ie_prices.append(ie_price)
+                slope_prices = np.array(ie_prices)[-Slopes.max_slope_length - 1:-1]
+                if slope_prices.shape[0] != Slopes.max_slope_length:
+                    continue
+
+                slope = Slope(prices=slope_prices)
+                make_trade = position.step(mark_price=ie_price, slope=slope)
+                if make_trade:
+                    if position.direction == PositionDirection.long:
+                        position.direction = PositionDirection.short
+                        binance_account.order(-1.5)
+                    else:
+                        position.direction = PositionDirection.long
+                        binance_account.order(2.5)
+
+                print(datetime.utcnow(), ie_prices[len(ie_prices) - 1], slope.length, slope.angle, position.direction)
+
+    margin_socket_manager = BinanceSocketManager(binance_client)
+    margin_socket_manager.start_depth_socket('BTCUSDT', process_depth_message,
+                                             depth=BinanceSocketManager.WEBSOCKET_DEPTH_5)
+    margin_socket_manager.start()
+
     while True:
         sleep(1)
 
-    initial_price = 40000.0
-    runner = LiveRunner(delta=0.001, initial_price=initial_price)
-
-    @ratelimit.limits(calls=1, period=1)
-    def print_it(msg):
-        # print("-> " + msg)
-        pass
-
-    position = PositionLive(delta=0.001, initial_price=initial_price, direction=PositionDirection.long)
-    ie_prices = deque(maxlen=Slopes.max_slope_length + 10)
-    ask, bid = initial_price, initial_price
-
-    while True:
-        if data_pipe.poll(0.5):
-            timestamp, price, buy = data_pipe.recv()
-            if buy:
-                ask = price
-            else:
-                bid = price
-
-            make_trade = None
-            new_ie_prices = runner.step(ask=ask, bid=bid)
-            if len(new_ie_prices) > 0:
-                for ie_price in new_ie_prices:
-                    ie_prices.append(ie_price)
-                    slope_prices = np.array(ie_prices)[-Slopes.max_slope_length - 1:-1]
-                    if slope_prices.shape[0] == Slopes.max_slope_length:
-                        slope = Slope(prices=slope_prices)
-                        make_trade = position.step(mark_price=ie_price, slope=slope)
-                        if make_trade:
-                            if position.direction == PositionDirection.long:
-                                position.direction = PositionDirection.short
-                                binance_account.order(-1)
-                            else:
-                                position.direction = PositionDirection.long
-                                binance_account.order(1)
-
-                        print(timestamp, ie_prices[len(ie_prices) - 1], slope.length, slope.angle, position.direction)
-
-            try:
-                print_it(f"trader has msg {timestamp.strftime('%Y-%m-%d %H:%M:%S.%f')} {ask:.1f} <-> {bid:.1f} {buy}")
-            except ratelimit.exception.RateLimitException:
-                pass
-
 
 if __name__ == '__main__':
-
-    live_data_pipe, trader_pipe = Pipe()
-
-    live_data_thread = threading.Thread(target=live_data, args=(live_data_pipe, ))
-    live_data_thread.start()
-
-    trader_thread = threading.Thread(target=trader, args=(trader_pipe, ))
-    trader_thread.start()
-
-    def signal_handler(signal_, frame):
-        live_data_pipe.send({'command': 'quit'})
-
-    signal.signal(signal.SIGINT, signal_handler)
-
-    live_data_thread.join()
-    trader_thread.join()
+    trader()
