cc2ce78d1cb115ed9eedc618d43d31eb4c3dab4d
Johan Strombom <strombom@gmail.com>
2021-03-15 17:48:24 +0100

    Scale data

--- BitBot/regime_trader.py
+++ BitBot/regime_trader.py
@@ -11,6 +11,11 @@ if __name__ == '__main__':
 
     lengths = pd.read_csv('../tmp/regime_data_lengths.csv')
     regime_data = pd.read_csv('../tmp/regime_data.csv')
+
+    regime_data['duration'] /= 100000000
+    regime_data['volume'] /= 2000
+    regime_data['delta'] /= 0.008
+
     regime_data_raw = regime_data.to_numpy()[:, 2:]
     print(regime_data_raw.shape)
 
@@ -23,18 +28,20 @@ if __name__ == '__main__':
     prices = []
     values = []
 
+
     for idx in range(n_timesteps - 1, runner_prices.shape[0]):
         features = regime_data_raw[idx - n_timesteps + 1:idx + 1, :].transpose()
-        prediction = regime_predictor.get_X_preds(features)[0]
-        print("p:", prediction)
+        print(features.shape)
+        quit()
+        prediction = regime_predictor.get_X_preds(features)[0][0]
 
         mark_price = runner_prices[idx]
 
-        if prediction > 0.4 and direction == -1:
+        if prediction[0] > 0.004 and direction == -1:
             order_size = simulator.calculate_order_size_btc(leverage=2.5, mark_price=mark_price)
             simulator.market_order(order_size_btc=order_size, mark_price=mark_price)
             direction = 1
-        elif prediction < -0.4 and direction == 1:
+        elif prediction[0] < -0.004 and direction == 1:
             order_size = simulator.calculate_order_size_btc(leverage=-1.5, mark_price=mark_price)
             simulator.market_order(order_size_btc=order_size, mark_price=mark_price)
             direction = -1
@@ -42,6 +49,7 @@ if __name__ == '__main__':
         value = simulator.get_value_usdt(mark_price=mark_price)
         prices.append(mark_price)
         values.append(value)
+        print("p:", prediction[0:4], mark_price, value)
 
     fig, axs = plt.subplots(2, 1)
     axs[0].plot(prices)
