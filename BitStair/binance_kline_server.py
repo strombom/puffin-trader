47883462d95119b86c6a48c6ba6c6ed8e9dabddd
Johan Strombom <strombom@gmail.com>
2021-04-29 19:38:35 +0200

    Move account to kline server

--- BitStair/binance_kline_server.py
+++ BitStair/binance_kline_server.py
@@ -7,8 +7,51 @@ from time import sleep
 from datetime import datetime, timedelta
 from collections import deque
 from binance.client import Client
+from binance.websockets import BinanceSocketManager
 
-from binance_account import BinanceAccount
+
+class BinanceKlineAccount:
+    def __init__(self, binance_client, top_symbols):
+        self.mark_prices = {}
+
+        self._client = binance_client
+        self._assets = {}
+        self._kline_threads = {}
+
+        all_symbols = {}
+        exchange_info = self._client.get_exchange_info()
+        for symbol in exchange_info['symbols']:
+            if symbol['baseAsset'] not in all_symbols:
+                all_symbols[symbol['baseAsset']] = []
+            all_symbols[symbol['baseAsset']].append(symbol['symbol'])
+
+        trade_symbols = []
+        for top_symbol in top_symbols:
+            if top_symbol in all_symbols:
+                for symbol in all_symbols[top_symbol]:
+                    if 'USDT' in symbol:
+                        trade_symbols.append(top_symbol)
+                        break
+
+        def process_trade_message(data):
+            if data['e'] == 'kline':
+                self.mark_prices[data['s']] = float(data['k']['c'])
+
+        for symbol in trade_symbols:
+            trade_socket_manager = BinanceSocketManager(self._client)
+            trade_socket_manager.start_kline_socket(symbol=symbol + "USDT", callback=process_trade_message, interval='1m')
+            trade_socket_manager.start()
+            self._kline_threads[symbol] = trade_socket_manager
+
+        from time import sleep
+        has_all_symbols = False
+        while not has_all_symbols:
+            has_all_symbols = True
+            for symbol in trade_symbols:
+                if symbol + "USDT" not in self.mark_prices:
+                    has_all_symbols = False
+                    sleep(0.2)
+                    break
 
 
 top_symbols = [
@@ -24,9 +67,6 @@ top_symbols = [
 'DCR', 'KSM', 'WAN', 'STX', 
 """
 
-top_symbols = [
-    'BTC', 'ETH', 'BNB'
-]
 
 def server():
     with open('binance_account.json') as f:
@@ -35,13 +75,13 @@ def server():
         api_secret = account_info['api_secret']
 
     binance_client = Client(api_key, api_secret)
-    binance_account = BinanceAccount(binance_client=binance_client, top_symbols=top_symbols)
+    binance_account = BinanceKlineAccount(binance_client=binance_client, top_symbols=top_symbols)
 
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:31007")
 
-    history = deque(maxlen=24 * 60)
+    history = deque(maxlen=7 * 24 * 60)
     history_counter = 0
 
     history_lock = threading.Lock()
@@ -49,11 +89,12 @@ def server():
     def history_thread():
         next_timestamp = datetime.now()
         while True:
-            next_timestamp += timedelta(minutes=60)
+            next_timestamp += timedelta(minutes=1)
             with history_lock:
                 nonlocal history_counter
                 history_counter = history_counter + 1
                 history.append(binance_account.mark_prices.copy())
+                print(f"{datetime.now()} Append: {binance_account.mark_prices}")
             while next_timestamp > datetime.now():
                 sleep(1.0)
 
