49c9af30adb59ff348c8d119d0b9256d7c4cdaa3
Johan Strombom <strombom@gmail.com>
2021-04-30 10:20:55 +0200

    Calculate directions

--- BitStair/live_trader.py
+++ BitStair/live_trader.py
@@ -1,50 +1,72 @@
+import zmq
 import json
-import threading
 import time
 import logging
+import threading
+import numpy as np
 from collections import deque
-
-import zmq
 from binance.client import Client
 
 from IntrinsicTime.runner import Runner
 
 
-class Runners:
+class Indicators:
     def __init__(self, config: dict):
-        self.config = config
+        self._config = config
         self._data = {}
+        self._directions = {}
         self._runners = {}
-        self.running = True
+        self._running = True
         self._updater_thread = threading.Thread(target=self._updater)
         self._updater_thread.start()
         self._updater_thread.join()
 
+    def _calculate_directions(self):
+        for pair in self._data:
+            self._directions[pair] = -1.0
+            prices = np.array(self._data[pair])
+            if prices.shape[0] != self._config['lengths'][-1]:
+                continue
+
+            for length_idx, length in enumerate(self._config['lengths']):
+                start, end = prices.shape[0] - length, prices.shape[0]
+                xp = np.arange(start, end)
+                yp = np.poly1d(np.polyfit(xp, prices[start:end], self._config['poly_order']))
+                curve = yp(xp)
+                direction = curve[-1] / curve[-2] - 1.0
+                self._directions[pair] = max(self._directions[pair], direction)
+
     def _updater(self):
         next_idx = 0
-        while self.running:
+
+        while self._running:
             context = zmq.Context()
             socket = context.socket(zmq.REQ)
-            socket.connect(self.config['binance_kline_server_address'])
+            socket.connect(self._config['binance_kline_server_address'])
             socket.send_pyobj(("get_since", next_idx))
             message = socket.recv_pyobj()
             next_idx = message['last_idx'] + 1
 
+            updated = False
             for prices in message['mark_prices']:
                 for pair in prices:
                     if pair not in self._data:
-                        self._runners[pair] = Runner(delta=self.config['delta'])
-                        self._data[pair] = deque(maxlen=self.config['lengths'][-1])
+                        self._runners[pair] = Runner(delta=self._config['delta'])
+                        self._data[pair] = deque(maxlen=self._config['lengths'][-1])
                     ie_prices = self._runners[pair].step(high=prices[pair], low=prices[pair])
                     for ie_price in ie_prices:
-                        print(f"Append ({pair}): {ie_price}")
+                        # print(f"Append ({pair}): {ie_price}")
                         self._data[pair].append(ie_price)
+                        updated = True
 
             # print(message['last_idx'], len(message['mark_prices']), len(self._data['ONEUSDT']))
 
-            for pair in self._data:
-                print(f"{len(self._data[pair])} ", end='')
-            print()
+            if updated:
+                self._calculate_directions()
+
+            # for pair in self._data:
+            #     print(f"{pair}: {len(self._data[pair])}, ", end='')
+            # print()
 
             time.sleep(1.0)
 
@@ -58,7 +80,7 @@ def trader():
     with open('config.json') as f:
         config = json.load(f)
 
-    runners = Runners(config)
+    indicators = Indicators(config)
 
     lengths = config['lengths']
     print(lengths)
