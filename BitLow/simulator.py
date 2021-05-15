cf0919bc0e7d3e998dfde0c608573ce31cd7e016
Johan Strombom <strombom@gmail.com>
2021-05-15 11:18:26 +0200

    Symbols of interest

--- BitLow/simulator.py
+++ BitLow/simulator.py
@@ -10,7 +10,7 @@ def main():
         data = pickle.load(f)
         print(data)
 
-    indicator_10_threshold = 0.0015
+    indicator_10_threshold = -0.0015
 
     #plt_dat = data['indicators'][12][10]
     #plt.hist(plt_dat, bins=150)
@@ -22,6 +22,14 @@ def main():
         for symbol_idx, symbol in enumerate(data['symbols']):
             simulator.set_mark_price(symbol=symbol, mark_price=data['prices'][symbol_idx][kline_idx])
 
+        symbols_of_interest = []
+        for symbol_idx, symbol in enumerate(data['symbols']):
+            val = data['indicators'][symbol_idx, 10, kline_idx]
+            if val < indicator_10_threshold:
+                symbols_of_interest.append((symbol_idx, val))
+        print(symbols_of_interest)
+        if len(symbols_of_interest) > 0:
+            print("ok")
 
 
 if __name__ == "__main__":
