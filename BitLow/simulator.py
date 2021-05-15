206105ddb8e19bb8fff0ac45010342237bc8e9f2
Johan Strombom <strombom@gmail.com>
2021-05-15 10:04:40 +0200

    Simulator

--- BitLow/simulator.py
+++ BitLow/simulator.py
@@ -1,6 +1,8 @@
 import pickle
 from matplotlib import pyplot as plt
 
+from BinanceSimulator.binance_simulator import BinanceSimulator
+
 
 def main():
 
@@ -10,9 +12,16 @@ def main():
 
     indicator_10_threshold = 0.0015
 
-    plt_dat = data['indicators'][12][10]
-    plt.hist(plt_dat, bins=150)
-    plt.show()
+    #plt_dat = data['indicators'][12][10]
+    #plt.hist(plt_dat, bins=150)
+    #plt.show()
+
+    simulator = BinanceSimulator(initial_usdt=1000, symbols=data['symbols'])
+
+    for kline_idx in range(data['prices'].shape[1]):
+        for symbol_idx, symbol in enumerate(data['symbols']):
+            simulator.set_mark_price(symbol=symbol, mark_price=data['prices'][symbol_idx][kline_idx])
+
 
 
 if __name__ == "__main__":
