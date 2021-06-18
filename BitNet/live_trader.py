b3659eb5c7aac9fdb59b0554ae7bedf4ea9c2ece
Johan Strombom <strombom@gmail.com>
2021-06-18 13:02:11 +0200

    Runners, Make directions

--- BitNet/live_trader.py
+++ BitNet/live_trader.py
@@ -1,8 +1,14 @@
+import collections
+import itertools
 
 import zmq
 import time
-import json
 import logging
+import numpy as np
+import pandas as pd
+from numba import jit
+
+from IntrinsicTime.runner import Runner
 
 
 def main():
@@ -11,11 +17,55 @@ def main():
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://localhost:31007")
 
-    command, payload = 'get_since', 1435
-    socket.send_pyobj((command, payload))
-    message = socket.recv_pyobj()
-    print(message['last_idx'])
-    print(message['prices'])
+    delta = 0.01
+    direction_degrees = [1, 2, 3]
+    lengths = pd.read_csv('cache/regime_data_lengths.csv')['length'].to_list()
+    step_count = lengths[-1]
+
+    # indicators = None
+    last_data_idx = 0
+    symbols = []
+    steps = {}
+    runners = {}
+
+    directions = None
+
+    while True:
+        command, payload = 'get_since', last_data_idx
+        socket.send_pyobj((command, payload))
+        message = socket.recv_pyobj()
+        last_data_idx = message['last_idx']
+        prices = message['prices']
+
+        if len(symbols) == 0:
+            symbols = sorted(prices[0].keys())
+            directions = np.empty((len(symbols), len(direction_degrees), len(lengths)))
+
+        # Runners
+        for price in prices:
+            for symbol in symbols:
+                if symbol not in runners:
+                    runners[symbol] = Runner(delta=delta)
+                    steps[symbol] = collections.deque(maxlen=step_count)
+                runner_steps = runners[symbol].step(price[symbol])
+                for step in runner_steps:
+                    steps[symbol].append(step)
+                    # print(symbol, step)
+
+        # Make directions
+        for symbol_idx, symbol in enumerate(symbols):
+            for direction_degree_idx, direction_degree in enumerate(direction_degrees):
+                for length_idx, length in enumerate(lengths):
+                    idx = lengths[-1]
+                    start, end = idx - length, idx
+                    xp = np.arange(start, end)
+                    direction_steps = list(itertools.islice(steps[symbol], start, end))
+                    yp = np.poly1d(np.polyfit(xp, direction_steps, direction_degree))
+                    curve = yp(xp)
+                    direction = curve[-1] / curve[-2] - 1.0
+                    directions[symbol_idx, direction_degree_idx, length_idx] = direction
+
+        quit()
 
 
 if __name__ == '__main__':
