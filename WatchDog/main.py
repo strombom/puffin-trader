10803ee34b6651b7904617925ea3c520ef87c5a9
Johan Strombom <strombom@gmail.com>
2021-06-29 23:04:51 +0200

    WatchDog

--- WatchDog/main.py
+++ WatchDog/main.py
@@ -1,21 +1,47 @@
+import json
 import time
-from datetime import datetime, timedelta
-
+import pickle
+import smtplib
+import threading
+import urllib.parse
 from flask import Flask
+from datetime import datetime, timedelta
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
 watchdogs = {}
 
 
+def save_watchdogs():
+    with open(f"wdg.pickle", 'wb') as f:
+        pickle.dump(watchdogs, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_watchdogs():
+    global watchdogs
+    try:
+        with open(f"wdg.pickle", 'rb') as f:
+            tmp_watchdogs = pickle.load(f)
+            for wdg_name in tmp_watchdogs:
+                tmp_watchdogs[wdg_name]['last_reset'] = datetime.now()
+            watchdogs = tmp_watchdogs
+
+    except FileNotFoundError:
+        pass
+
+
 @app.route('/<wdg_name>')
 def reset_wdg(wdg_name):
     if wdg_name not in watchdogs:
         watchdogs[wdg_name] = {
-            'period': 90,
+            'period': timedelta(seconds=90),
             'ongoing_alarm': False
         }
-    watchdogs['last_reset'] = datetime.now()
+    watchdogs[wdg_name]['last_reset'] = datetime.now()
+    save_watchdogs()
+    return f"Reset wdg_name<br><br>{watchdogs}"
 
 
 @app.route('/<wdg_name>/<timeout_time>')
@@ -42,8 +68,28 @@ def setup_wdg(wdg_name, timeout_time):
         return f"Fail"
 
 
+@app.route('/status')
+def status_wdg():
+    return f"Status<br><br>{watchdogs}"
+
+
 def send_email(name):
-    print(name)
+    subject = f"BitBot Watchdog Alert {name}"
+    message = f"BitBot Watchdog Alert {name}"
+
+    with open('mail_account.json') as f:
+        account_info = json.load(f)
+
+    port_number = account_info['account_port']
+    msg = MIMEMultipart()
+    msg['From'] = account_info['email_from']
+    msg['To'] = account_info['email_to']
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message))
+    mailserver = smtplib.SMTP('localhost', port_number)
+    mailserver.login(account_info['account_email'], account_info['account_password'])
+    mailserver.sendmail(account_info['email_from'], account_info['email_to'], msg.as_string())
+    mailserver.quit()
 
 
 def wdg_loop():
@@ -54,7 +100,11 @@ def wdg_loop():
             if not watchdog['ongoing_alarm'] and datetime.now() > watchdog['last_reset'] + watchdog['period']:
                 watchdog['ongoing_alarm'] = True
                 send_email(wdg_name)
+                print(f"Watchdog alarm {wdg_name}")
 
 
 if __name__ == '__main__':
-    app.run()
+    load_watchdogs()
+    x = threading.Thread(target=wdg_loop)
+    x.start()
+    app.run(host='localhost', port=31008)
