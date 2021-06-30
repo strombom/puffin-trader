7d00cf8c8eb76166bcb294ab275506308672f627
Johan Strombom <strombom@gmail.com>
2021-06-30 22:38:49 +0200

    Rename steps

--- BitNet/make_indicators.py
+++ BitNet/make_indicators.py
@@ -38,29 +38,29 @@ def make_indicator(lock, task_queue):
 
         symbol = parameters['symbol']
         lengths = parameters['lengths']
-        start_timestamp = parameters['start_timestamp']
-        end_timestamp = parameters['end_timestamp']
+        #start_timestamp = parameters['start_timestamp']
+        #end_timestamp = parameters['end_timestamp']
         direction_degrees = parameters['direction_degrees']
         n_timesteps = parameters['n_timesteps']
 
         indicators = np.zeros((len(lengths), len(direction_degrees), n_timesteps))
 
-        timestamps = np.array(intrinsic_events[symbol]['timestamps'])
-        prices = np.array(intrinsic_events[symbol]['steps'])
+        steps_idx = np.array(intrinsic_events[symbol]['klines_idxs'])
+        steps_price = np.array(intrinsic_events[symbol]['steps_price'])
 
         print(f"Processing {symbol}")
 
         for direction_degree in direction_degrees:
-            directions = np.zeros((len(lengths), timestamps.shape[0]))
+            directions = np.zeros((len(lengths), steps_idx.shape[0]))
 
             make_spectrum(lengths=lengths,
-                          prices=prices,
+                          prices=steps_price,
                           poly_order=direction_degree,
                           directions=directions)
 
             direction_idx = 0
-            for indicator_idx in range(timestamps[0], n_timesteps):
-                while indicator_idx > timestamps[direction_idx] and direction_idx + 1 < len(timestamps):
+            for indicator_idx in range(steps_idx[0], n_timesteps):
+                while indicator_idx > steps_idx[direction_idx] and direction_idx + 1 < len(steps_idx):
                     direction_idx += 1
                 indicators[:, direction_degree - 1, indicator_idx] = directions[:, direction_idx]
 
@@ -79,7 +79,7 @@ def main():
         intrinsic_events = pickle.load(f)
 
     start_timestamp = datetime.strptime("2020-01-01 00:00:00", "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
-    end_timestamp = datetime.strptime("2021-06-19 00:00:00", "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
+    end_timestamp = datetime.strptime("2021-06-29 00:00:00", "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
 
     direction_degrees = [1, 2, 3]
     lengths = np.array([5, 7, 11, 15, 22, 33, 47, 68, 100])
@@ -94,8 +94,8 @@ def main():
     for symbol in intrinsic_events:
         task_queue.put({
             'symbol': symbol,
-            'start_timestamp': start_timestamp,
-            'end_timestamp': end_timestamp,
+            #"'start_timestamp': start_timestamp,
+            #'end_timestamp': end_timestamp,
             'lengths': list(lengths),
             'direction_degrees': direction_degrees,
             'n_timesteps': n_timesteps
