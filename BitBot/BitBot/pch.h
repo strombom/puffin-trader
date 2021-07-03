14c1bf1042679ce48fb6250b954ca63511d6a378
Johan Strombom <strombom@gmail.com>
2021-07-03 14:12:40 +0200

    Project configuration

--- BitBot/BitBot/pch.h
+++ BitBot/BitBot/pch.h
@@ -1,4 +1,9 @@
 #pragma once
 
+#pragma warning(push)
+#pragma warning(disable: 4244)
+#pragma warning(disable: 4018)
 #include "BitLib/csv/csv.h"
+#pragma warning(pop)
+
 #include "BitLib/date/date.h"
