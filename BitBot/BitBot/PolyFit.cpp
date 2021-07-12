fa718bb30905d1680d40e7339485bc1c9383101e
Johan Strombom <strombom@gmail.com>
2021-07-12 11:08:38 +0200

    New polyfit algorithm

--- BitBot/BitBot/PolyFit.cpp
+++ BitBot/BitBot/PolyFit.cpp
@@ -2,221 +2,120 @@
 
 #include "PolyFit.h"
 
-// https://github.com/gullibility/leastsquare
+#include <filesystem>
+#include <fstream>
 
 
-PolyFit::PolyFit(void) : degree(degree), length(length)
+
+PolyFit::PolyFit(void)
 {
-    for (auto i = 0; i < max_length; i++) {
-        x[i] = i;
-    }
-}
 
-bool PolyFit::matrix_lu(void)
-{
-    for (auto i = 0; i < polyfit_n; i++) {
-        for (auto j = 0; j < polyfit_n; j++) {
-            L[i * polyfit_n + j] = U[i * polyfit_n + j] = 0;
-        }
-        U[i * polyfit_n + i] = 1;
-    }
-    for (auto j = 0, d = 0; j < polyfit_n; j++) {
-        for (auto i = j, b = d; i < polyfit_n; i++) {
-            auto temp = 0.0;
-            auto a = 0, c = j;
-            while (a < j) {
-                temp += L[b + a] * U[c];
-                c += polyfit_n;
-                a++;
-            }
-            L[b + j] = Kmul[b + j] - temp;
-            b += polyfit_n;
-        }
-        auto i = j + 1;
-        while (i < polyfit_n) {
-            auto temp = 0.0;
-            auto a = 0, c = i;
-            while (a < j) {
-                temp += L[d + a] * U[c];
-                a++;
-                c += polyfit_n;
-            }
-            if (L[d + j] == 0) {
-                return false;
-            }
-            U[d + i] = (Kmul[d + i] - temp) / L[d + j];
-            i++;
-        }
-        d += polyfit_n;
-    }
-    return true;
-}
-
-bool PolyFit::matrix_inv(void)
-{
-    if (!matrix_lu()) {
-        return false;
-    }
-
-    auto a = polyfit_n * polyfit_n;
-    for (auto i = 0; i < polyfit_n; i++) {
-        ix[i] = id[i] = 0;
-    }
-    for (auto i = 0; i < polyfit_n; i++) {
-        auto j = 0;
-        for (j = 0; j < polyfit_n; j++) {
-            ie[j] = 0;
-        }
-        ie[i] = 1;
-        j = 0;
-        auto b = 0;
-        while (j < polyfit_n) {
-            auto temp = 0.0;
-            a = 0;
-            while (a < j) {
-                temp += id[a] * L[b + a];
-                a++;
-            }
-            id[j] = ie[j] - temp;
-            id[j] /= L[b + j];
-            j++;
-            b += polyfit_n;
-        }
-        j = polyfit_n - 1;
-        b -= polyfit_n;
-        while (j > -1) {
-            auto temp = 0.0;
-            a = j + 1;
-            while (a < polyfit_n) {
-                temp += U[b + a] * ix[a];
-                a++;
-            }
-            ix[j] = id[j] - temp;
-            ix[j] /= U[b + j];
-            j--;
-            b -= polyfit_n;
-        }
-        for (j = 0, b = i; j < polyfit_n; j++) {
-            Kinv[b] = ix[j];
-            b += polyfit_n;
-        }
-    }
-    return true;
-}
-
-void PolyFit::matrix_mul_a(void)
-{
-    for (auto i = 0; i < polyfit_n; i++) {
-        for (auto j = 0; j < polyfit_n; j++) {
-            const auto b = i * polyfit_n + j;
-            Kmul[b] = 0;
-            for (auto k = 0; k < polyfit_n; k++) {
-                Kmul[b] += KT[i * polyfit_n + k] * x2[k * polyfit_n + j];
-            }
-        }
-    }
-}
-
-void PolyFit::matrix_mul_b(void)
-{
-    for (auto i = 0; i < polyfit_n; i++) {
-        Kb[i] = 0;
-        for (auto k = 0; k < polyfit_n; k++) {
-            Kb[i] += KT[i * polyfit_n + k] * y2[k];
-        }
-    }
-}
-
-void PolyFit::matrix_mul_c(void)
-{
-    for (auto i = 0; i < polyfit_n; i++) {
-        ks[i] = 0;
-        for (auto k = 0; k < polyfit_n; k++) {
-            ks[i] += Kinv[i * polyfit_n + k] * Kb[k];
-        }
-    }
-}
-
-void PolyFit::matrix_t(void)
-{
-    for (auto i = 0, a = 0; i < polyfit_n; i++) {
-        for (auto j = 0, b = 0; j < polyfit_n; j++) {
-            KT[b + i] = x2[a + j];
-            b += polyfit_n;
-        }
-        a += polyfit_n;
-    }
-}
-
-bool PolyFit::matrix_solve(void)
-{
-    matrix_t();
-    matrix_mul_a();
-    matrix_mul_b();
-    if (matrix_inv()) {
-        matrix_mul_c();
-        return true;
-    }
-    return false;
 }
 
 std::tuple<double, double> PolyFit::calculate_direction(std::array<double, max_length> price_steps, int degree, int length)
 {
-    this->degree = degree;
-    this->length = length;
-    polyfit_n = degree + 1;
+    // https://gist.github.com/chrisengelsma/108f7ab0a746323beaaf7d6634cf4add
 
-    for (auto i = 0, idx = 0; i < polyfit_n; i++) {
-        y2[i] = 0;
-        for (auto j = 0; j < polyfit_n; j++) {
-            x2[idx + j] = 0;
+    const auto degree_p1 = degree + 1;
+    const auto degree_t2p1 = degree * 2 + 1;
+
+    // X = sigma(xi^2n)
+    for (int i = 0; i < degree_t2p1; ++i) {
+        X[i] = 0;
+        for (int x = 0; x < length; ++x) {
+            X[i] += std::pow(x, i);
         }
-        idx += polyfit_n;
     }
 
-    x2[0] = length;
-    for (auto i = 0; i < polyfit_n; i++) {
-        for (auto j = i + 1; j < polyfit_n; j++) {
-            auto temp = 0.0;
-            auto idx = 0;
-            for (idx = 0; idx < length; idx++) {
-                temp += std::pow(x[idx], i + j);
-            }
-            idx = j;
-            for (auto n = i; n < polyfit_n; n++) {
-                if (idx >= 0) {
-                    x2[n * polyfit_n + idx] = temp;
+    // B = normal augmented matrix that stores the equations
+    for (int i = 0; i <= degree; ++i) {
+        for (int j = 0; j <= degree; ++j) {
+            B[i][j] = X[i + j];
+        }
+    }
+
+    // Y = sigma(xi^n * yi)
+    for (int i = 0; i < degree_p1; ++i) {
+        Y[i] = 0.0;
+        for (int x = 0; x < length; ++x) {
+            Y[i] += std::pow(x, i) * price_steps[max_length - length + x];
+        }
+    }
+
+    // Load values of Y as last column of B
+    for (int i = 0; i <= degree; ++i) {
+        B[i][degree_p1] = Y[i];
+    }
+
+    // Pivotisation of the B matrix.
+    for (int i = 0; i < degree_p1; ++i) {
+        for (int k = i + 1; k < degree_p1; ++k) {
+            if (B[i][i] < B[k][i]) {
+                for (int j = 0; j <= degree_p1; ++j) {
+                    const auto tmp = B[i][j];
+                    B[i][j] = B[k][j];
+                    B[k][j] = tmp;
                 }
-                idx--;
             }
         }
     }
 
-    auto temp = 0.0;
-    for (auto i = 0; i < length; i++) {
-        temp += std::pow(x[i], polyfit_n * 2 - 2);
-    }
-    x2[polyfit_n * polyfit_n - 1] = temp;
-    for (auto i = 0; i < polyfit_n; i++) {
-        temp = 0;
-        for (auto j = 0; j < length; j++) {
-            temp += price_steps[max_length - length + j] * std::pow(x[j], i);
+    // Performs the Gaussian elimination.
+    for (int i = 0; i < degree; ++i) {
+        for (int k = i + 1; k < degree_p1; ++k) {
+            const auto t = B[k][i] / B[i][i];
+            for (int j = 0; j <= degree_p1; ++j) {
+                // Make all elements below the pivot equals to zero
+                //  or eliminate the variable.
+                B[k][j] -= t * B[i][j];
+            }
         }
-        y2[i] = temp;
     }
 
-    if (matrix_solve()) {
-        // Todo: calculate direction from ks
-
-        auto p0 = ks.at(0);
-        auto p1 = ks.at(0);
-        for (auto i = 1; i < polyfit_n; i++) {
-            p0 += ks[i] * std::pow(x[length - 2], i);
-            p1 += ks[i] * std::pow(x[length - 1], i);
+    // Back substitution.
+    for (int i = degree; i >= 0; --i) {
+        // Set the variable as the rhs of last equation
+        coeffs[i] = B[i][degree_p1];
+        for (int j = 0; j < degree_p1; ++j) {
+            if (j != i) {
+                // Subtract all lhs values except the target coefficient.
+                coeffs[i] -= B[i][j] * coeffs[j];
+            }
         }
-
-        return { p0, p1 };
+        // Divide rhs by coefficient of variable being calculated.
+        coeffs[i] /= B[i][i];
     }
 
-    return { 0.0, 0.0 };
+    /*
+    auto file_path = std::string{ BitBot::path } + "\\polyfit";
+    std::filesystem::create_directories(file_path);
+    file_path += "\\" + std::to_string(degree) + "-" + std::to_string(length) + ".csv";
+
+    auto csv_file = std::ofstream{ file_path, std::ios::binary };
+
+    csv_file << "\"x\";\"p\";\"y\"\n";
+
+    for (auto x = 0; x < length; x++) {
+        csv_file << x << ";";
+        csv_file << price_steps[max_length - length + x] << ";";
+
+        auto y = 0.0;
+        for (auto i = 0; i < degree_p1; i++) {
+            y += coeffs[i] * std::pow(x, i);
+        }
+        csv_file << y << "\n";
+    }
+
+    csv_file.close();
+    */
+
+    // Result
+    auto p0 = 0.0;
+    auto p1 = 0.0;
+    for (auto i = 0; i < degree_p1; i++) {
+        p0 += coeffs[i] * std::pow(length - 2, i);
+        p1 += coeffs[i] * std::pow(length - 1, i);
+    }
+
+    return { p0, p1 };
 }
--- BitBot/BitBot/PolyFit.h
+++ BitBot/BitBot/PolyFit.h
@@ -14,33 +14,9 @@ public:
 
     std::tuple<double, double> calculate_direction(std::array<double, max_length> price_steps, int degree, int length);
 
-    bool matrix_solve(void);
-    void matrix_t(void);
-    void matrix_mul_a(void);
-    void matrix_mul_b(void);
-    void matrix_mul_c(void);
-    bool matrix_inv(void);
-    bool matrix_lu(void);
-
 private:
-    int degree;
-    int length;
-    int polyfit_n;
-
-    std::array<double, max_length> x;
-    std::array<double, max_polyfit_n * max_polyfit_n> x2;
-    std::array<double, max_polyfit_n> y2;
-    std::array<double, max_polyfit_n> ks;
-
-    std::array<double, max_polyfit_n * max_polyfit_n> KT;
-    std::array<double, max_polyfit_n * max_polyfit_n> Kmul;
-    std::array<double, max_polyfit_n * max_polyfit_n> Kinv;
-    std::array<double, max_polyfit_n> Kb;
-
-    std::array<double, max_polyfit_n * max_polyfit_n> L;
-    std::array<double, max_polyfit_n * max_polyfit_n> U;
-
-    std::array<double, max_polyfit_n> id;
-    std::array<double, max_polyfit_n> ix;
-    std::array<double, max_polyfit_n> ie;
+    std::array<double, max_degree_p1> coeffs;
+    std::array<double, max_degree_t2p1> X;
+    std::array<double, max_degree_p1> Y;
+    std::array<std::array<double, max_degree_p2>, max_degree_p1> B;
 };
