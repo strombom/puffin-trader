2b3d00185710e7adf9a39de540bdb3d1f13f78a7
strombom <strombom@gmail.com>
2021-10-08 13:23:38 +0200

    Daily bins

--- BitNet/evaluate_predictions.py
+++ BitNet/evaluate_predictions.py
@@ -1,60 +1,104 @@
 import os
 import glob
-
 import torch
 import pickle
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib import cm
 
 
+def calc_up_down(p):
+    ups_, downs_ = (p > 0.0).nonzero().squeeze(), (p < 0.0).nonzero().squeeze()
+    up_count, down_count = 0, 0
+    if len(downs_.shape) > 0:
+        down_count = downs_.shape[0]
+    if len(ups_.shape) > 0:
+        up_count = ups_.shape[0]
+    return up_count, down_count
+
+
+def print_up_down(up_count, down_count):
+    n = 4
+    if down_count + up_count == 0:
+        print(f"  {up_count:n} up, {down_count:n} down, tot {(up_count + down_count):n}, ratio -", end='')
+    else:
+        print(
+            f"  {up_count:n} up, {down_count:n} down, tot {(up_count + down_count):n}, ratio {up_count / (up_count + down_count):.2f}",
+            end='')
+
+
 def main():
-    def calc_up_down(p):
-        ups_ = (p > 0.0).nonzero().squeeze()
-        downs_ = (p < 0.0).nonzero().squeeze()
-
-        if len(downs_.shape) == 0:
-            down_count = 0
-        else:
-            down_count = downs_.shape[0]
-
-        if len(ups_.shape) == 0:
-            up_count = 0
-        else:
-            up_count = ups_.shape[0]
-
-        return up_count, down_count
-
-    def print_up_down(up_count, down_count):
-        if down_count + up_count == 0:
-            print(f"  {up_count} up, {down_count} down, tot {up_count + down_count}, ratio -", end='')
-        else:
-            print(f"  {up_count} up, {down_count} down, tot {up_count + down_count}, ratio {up_count / (up_count + down_count):.2f}", end='')
-
     bins = 20
+    stats, dates, all_stats = [], [], {}
+    ptm, pvm, gttm, gtvm, cnt = 0, 0, 0, 0, 0
+    y_idx = 6
 
-    stats = []
-    dates = []
-    all_stats = {}
-    y_idx = 1
+    training_path = 'E:/BitBot/simulation_data/'
 
-    ptm = 0
-    pvm = 0
-    gttm = 0
-    gtvm = 0
-    cnt = 0
+    file_path = f"cache/predictions.pickle"
+    try:
+        with open(file_path, 'rb') as f:
+            prediction_data = pickle.load(f)
+            symbols = prediction_data['symbols']
+            timestamps = prediction_data['timestamps']
+            predictions = prediction_data['predictions']
+            prediction_indices = prediction_data['prediction_indices']
+            ground_truths = prediction_data['ground_truths']
+    except FileNotFoundError:
+        print("Load predictions fail ", file_path)
+        quit()
 
+    bin_count = 40
+
+    timestamp_start = timestamps[0].replace(hour=0, minute=0, second=0)
+    timestamp_end = timestamps[-1].replace(hour=0, minute=0, second=0)
+    daily_bins = []
+    for day_idx in range((timestamp_end - timestamp_start).days):
+        daily_bins.append({
+            'up': [0 for n in range(bin_count)],
+            'down': [0 for n in range(bin_count)]
+        })
+
+    for symbol in symbols:
+        for idx in prediction_indices[symbol]:
+            day_idx = (timestamps[idx] - timestamp_start).days
+            bin_idx = int((predictions[symbol][idx][y_idx] + 2) * 10)
+            if 0 <= bin_idx < bin_count:
+                if ground_truths[symbol][idx][y_idx] == 1:
+                    daily_bins[day_idx]['up'][bin_idx] += 1
+                else:
+                    daily_bins[day_idx]['down'][bin_idx] += 1
+
+    #print(up_bins)
+    #print(down_bins)
+    for bin_idx in range(bin_count - 1, -1, -1):
+        print(f"Bin {bin_idx / 10 - 2:2.3f}: ", end='')
+        print_up_down(up_count=up_bins[bin_idx], down_count=down_bins[bin_idx])
+        print()
+
+    quit()
+
+    # Load ground truth
+    for file_path in glob.glob(training_path + "*.csv"):
+        symbol = file_path.split("_")[-1].replace(".csv", "")
+        data = pd.read_csv(file_path, index_col='ind_idx', parse_dates=['timestamp'], infer_datetime_format=True)
+        print(data)
+
+        break
+
+    quit()
     started = False
     for file_path in glob.glob("preds/preds_*.pickle"):
         #if "2020-01-07" in file_path:
         #    break
 
-        #if "2020-03-01" in file_path:
-        #    started = True
-        #if "2020-03-27" in file_path:
+        if "2020-01-15" in file_path:
+            started = True
+        #if "2020-05-15" in file_path:
         #    break
-        #if not started:
-        #    continue
+        if not started:
+            continue
 
         date = file_path.split('_')[1].split('.')[0]
         dates.append(date)
@@ -63,7 +107,6 @@ def main():
 
         with open(file_path, 'rb') as f:
             predictions = pickle.load(f)
-
 
         pred_train_mean = predictions['pred_train'][:, y_idx].mean()
         gt_train_mean = predictions['gt_train'][:, y_idx].double().mean()
@@ -79,8 +122,8 @@ def main():
 
         stat = {}
         for i in range(bins - 1, -bins - 1, -1):
-            minval_a = (i + 0) / bins
-            minval_b = (i + 1) / bins
+            minval_a = (i + 0) / bins * 2
+            minval_b = (i + 1) / bins * 2
 
             #minval_a = gt_train_mean - (i + 1) / 20
             #minval_b = gt_train_mean - (i + 0) / 20
@@ -133,7 +176,6 @@ def main():
             #print()
             all_stats[minval_a]['val_up'] += ups
             all_stats[minval_a]['val_down'] += downs
-
 
         stats.append(stat)
         #break
