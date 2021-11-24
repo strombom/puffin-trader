5ac3fcf0fc39d94d2f2cbc0a8c4ba85817b777c0
strombom <strombom@gmail.com>
2021-11-24 12:08:33 +0100

    Parse order

--- BitBackend/BitBackend/ByBitWebsocket.cpp
+++ BitBackend/BitBackend/ByBitWebsocket.cpp
@@ -130,7 +130,34 @@ void ByBitWebSocket::parse_message(std::string *message)
         return;
     }
 
-    auto a = 1;
+    const std::string_view topic = doc["topic"];
+
+    if (topic == "order") {
+        for (auto order : doc["data"]) {
+            const std::string_view id_str = order["order_link_id"];
+            if (id_str.size() == 36) {
+                const auto id = Uuid{ id_str };
+                const auto symbol = string_to_symbol(order["symbol"]);
+                const auto side = string_to_side(order["side"]);
+                const auto price = double{ order["price"] };
+                const auto qty = double{ order["leaves_qty"] };
+                std::string timestamp_str = std::string_view{ order["create_time"] }.data();
+                if (timestamp_str.size() > 26) {
+                    timestamp_str[26] = 'Z';
+                }
+                const auto timestamp = DateTime::iso8601_us_to_time_point_us(timestamp_str);
+                const auto confirmed = false;
+                order_manager->portfolio->update_order(id, symbol, side, qty, price, timestamp, confirmed);
+                order_manager->order_updated();
+            }
+        }
+    }
+    else if (topic == "position") {
+        // "{\"topic\":\"position\",\"action\":\"update\",\"data\":[{\"user_id\":\"2363961\",\"symbol\":\"BTCUSDT\",\"size\":0,\"side\":\"Buy\",\"position_value\":0,\"entry_price\":0,\"liq_price\":0,\"bust_price\":0,\"leverage\":100,\"order_margin"...
+    }
+    else {
+        auto a = 1;
+    }
 
 
     //auto error_message = std::string{ "{\"command\":\"error\"}" };
